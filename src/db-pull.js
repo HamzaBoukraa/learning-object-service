99acd850e6ad5f65e5bab3c0c94f026c7ea09f1c
Kyle Sherbert <ksherb1@students.towson.edu>
2017-10-01 22:57:01 -0400

    Write LO suggestion service using websockets
    lo-suggestion is now written, using mongo's text search. Still not convinced it's a good algorithm, but it's very easy to use.
    
    I also tweaked db-pull, 'cause it was...um, broken, in my last commit. Not that it matters - it's just a test file
    
    Contributes to #2

--- src/db-pull.js
+++ src/db-pull.js
@@ -1,16 +1,16 @@
 // this script will pull data from BloominOnion app and insert it into our MongoDB
 
-// (but right now it's just proving that the db.service works)
+// (but right now it's just proving that the db.driver works)
 
 
-var db = require('./db.service');
+var db = require('./db.driver');
 
 db.connect((err) => {
     if(err) throw err;
 
     db.insert('standardLO', {
-        outcome: "this is a test again",
-        criterion: 15
+        outcome: "this is a complete test",
+        criterion: "CAE KU 18"
     });
 
     db.disconnect();
--- src/lo-suggestion.js
+++ src/lo-suggestion.js
@@ -1,1 +1,61 @@
-// I will write this in short order
+var app = require('express')()
+var http = require('http').Server(app);
+// TODO: ^- I don't know if we really need express to initialize http..?
+var io = require('socket.io')(http);
+var db = require('./db.driver');
+
+var threshold = 1.1;    // minimum text score to be suggested
+
+db.connect((err) => {
+    if(err) throw err;
+
+
+    io.on('connection', function(socket) {
+        // what to do when client sends learning outcome text
+        socket.on('outcome', function(outcome) {
+            let cursor = db.find('standardLO',
+                    { $text: {$search: outcome} },
+                    { score: {$meta: "textScore"} })
+                .sort( { score: {$meta: "textScore"} } ) ;
+            
+            let suggestions = [];
+
+            cursor.forEach((doc) => {
+                if(doc.score < threshold) return false;
+                // TODO: ^- this ensures weak results aren't communicated, but
+                //  I'd really like it to just terminate the iteration.
+                //  I think we need to use the cursor's "hasNext" and "next" operations,
+                //      but the documentation is sloppy and I'm not sure how to.
+                suggestions.push({
+                    outcome: doc.outcome,
+                    criterion: doc.criterion
+                });
+            }, (err) => {
+                if(err) throw err;
+                socket.emit('suggestion', suggestions);
+
+                // TODO: the next couple bits are just for testing
+                console.log("--- Server --- "+outcome);
+                console.log(suggestions);
+            });
+        });
+    });
+});
+
+// TODO: not sure if this http.listen is really how we want this service to work
+http.listen(3000, ()=> {
+    // TODO: replace this whole callback with an actual client program ;)
+    // testing space
+    var client = require('socket.io-client')("http://localhost:3000");   // for testing purposes only
+    function testit(outcome) {
+        console.log("Client: sending "+outcome);
+        client.emit('outcome', outcome);
+    }
+    testit("test");
+    testit("complete");
+    testit("complete test");
+    // So, the text search feature of mongo is certainly convenient,
+    // but I'm not sure yet how it actually works. Just searching "complete"
+    // yields nothing, yet the word certainly affects things in "complete test".
+    // I admit there's a part of me that wants to learn about it just so I can write my own...
+});
