7be37e8244d6d1cf7ccc54e99dc138e79d8aeaec
Kyle Sherbert <ksherb1@students.towson.edu>
2017-10-08 11:55:26 -0400

    Set up mysql script to pull data from heroku
    Script still needs actual credentials, and also the code to actually parse sql data into our mongodb schema.
    
    Contributes to #1

--- src/db-pull.js
+++ src/db-pull.js
@@ -1,17 +1,37 @@
 // this script will pull data from BloominOnion app and insert it into our MongoDB
 
-// (but right now it's just proving that the db.driver works)
-
-
 var db = require('./db.driver');
+
+// TODO: replace the following parameters with values from a config file
+var heroku = require('mysql').createConnection({
+    host: "tbd",
+    user: "tbd",
+    password: "tbd",
+    database: "tbd"
+});
 
 db.connect((err) => {
     if(err) throw err;
 
-    db.insert('standardLO', {
-        outcome: "this is a complete test",
-        criterion: "CAE KU 18"
-    });
+    heroku.connect();
 
-    db.disconnect();
+    var query = connection.query('SELECT * FROM tbd');  // TODO: I need the table names
+    query
+        .on('error', (err) => {
+            console.log("Problem with heroku query: "+err);
+        })
+        .on('fields', (fields) => {
+            // TODO: I doubt I need to do anything on this event; if so get rid of it
+        })
+        .on('result', (row) => {
+            // TODO: parse data string into object
+            // TODO: grab each LO, LG, etc. and adorn as necessary
+            // TODO: insert into database
+        })
+        .on('end', () => {
+            db.disconnect();
+        });
+    // TODO: repeat process for user table
+    
+    heroku.end();   // automatically queued, will not end connection until query is buffered
 })
