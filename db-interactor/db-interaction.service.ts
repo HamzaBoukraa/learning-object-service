a7b78a82b7c3ebea8e948d364119f686ace5ee0a
Kyle Sherbert <ksherb1@students.towson.edu>
2017-11-13 19:27:49 -0500

    Convert db-interactor socket.io events to GET requests

--- db-interactor/db-interaction.service.ts
+++ db-interactor/db-interaction.service.ts
@@ -1,6 +1,7 @@
 require('../useme');
 
-import * as server from 'socket.io';
+import * as express from 'express';
+const bodyParser = require('body-parser');
 
 import * as db from '../db.driver';
 import * as glue from '../db.gluer';
@@ -18,116 +19,133 @@ import {
 /*
  * TODO: catch errors gracefully, preferably with logging!
  */
+const server = express();
+server.use(bodyParser.json());
 
 db.connect(process.env["CLARK_DB_URI"])
   .then(() => {
-    let io = server.listen(process.env["CLARK_DB_INTERACTOR_PORT"]);
-    console.log("Listening on port "+process.env["CLARK_DB_INTERACTOR_PORT"]);
-
-    io.on('connection', function(socket) {
-
-        socket.on('authenticate', (userid: string, pwd: string, ack: (res:boolean)=>void) => {
-            glue.authenticate(userid, pwd)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('findUser', (userid: string, ack: (res:UserID)=>void ) => {
-            db.findUser(userid)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('findLearningObject', (author: UserID, name: string, ack: (res:LearningObjectID)=>void ) => {
-            db.findLearningObject(author, name)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('loadUser', (id: UserID, ack: (res:User)=>void) => {
-            glue.loadUser(id)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('loadLearningObjectSummary', (id: UserID, ack: (res:LearningObject[])=>void) => {
-            glue.loadLearningObjectSummary(id)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('loadLearningObject', (id: LearningObjectID, ack: (res:LearningObject)=>void) => {
-            glue.loadLearningObject(id)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('addUser', (user: User, ack: (res:UserID)=>void) => {
-            glue.addUser(user)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('addLearningObject', (author: UserID, object: LearningObject, ack: (res:LearningObjectID)=>void) => {
-            glue.addLearningObject(author, object)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('addLearningOutcome', (source: LearningObjectID, outcome: LearningOutcome, ack: (res:LearningOutcomeID)=>void) => {
-            glue.addLearningOutcome(source, outcome)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('editUser', (id: UserID, user: User, ack: (res:void)=>void) => {
-            glue.editUser(id, user)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('editLearningObject', (id: LearningObjectID, object: LearningObject, ack: (res:void)=>void) => {
-            glue.editLearningObject(id, object)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('editLearningOutcome', (id: LearningOutcomeID, outcome: LearningOutcome, ack: (res:void)=>void) => {
-            glue.editLearningOutcome(id, outcome)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('updateLearningObject', (id: LearningObjectID, object: LearningObject, ack: (res:void)=>void) => {
-            glue.updateLearningObject(id, object)
-                .then((res)=>{ack(res)});
-        });
-        
-        socket.on('reorderObject', (user: UserID, object: LearningObjectID, index: number, ack: (res:void)=>void) => {
-            db.reorderObject(user, object, index)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('reorderOutcome', (object: LearningObjectID, outcome: LearningOutcomeID, index: number, ack: (res:void)=>void) => {
-            db.reorderOutcome(object, outcome, index)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('unmapOutcome', (outcome: LearningOutcomeID, mapping: OutcomeID, ack: (res:void)=>void) => {
-            db.unmapOutcome(outcome, mapping)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('mapOutcome', (outcome: LearningOutcomeID, mapping: OutcomeID, ack: (res:void)=>void) => {
-            db.mapOutcome(outcome, mapping)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('deleteUser', (id: UserID, ack: (res:void)=>void ) => {
-            db.deleteUser(id)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('deleteLearningObject', (id: LearningObjectID, ack: (res:void)=>void ) => {
-            db.deleteLearningObject(id)
-                .then((res)=>{ack(res)});
-        });
-
-        socket.on('deleteLearningOutcome', (id: LearningOutcomeID, ack: (res:void)=>void ) => {
-            db.deleteLearningOutcome(id)
-                .then((res)=>{ack(res)});
-        });
-
+    server.get('/authenticate', (req, res) => {
+        glue.authenticate(req.body.userid, req.body.pwd)
+            .then((authorized)=>{ res.send(authorized); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/findUser', (req, res) => {
+        db.findUser(req.body.userid)
+            .then((id)=>{ res.send(id); })
+            .catch((err)=>{ res.send({error:err}); });
     });
 
+    server.get('/findLearningObject', (req, res) => {
+        db.findLearningObject(req.body.author, req.body.name)
+            .then((id)=>{ res.send(id); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/loadUser', (req, res) => {
+        glue.loadUser(req.body.id)
+            .then((user)=>{ res.send(user); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/loadLearningObjectSummary', (req, res) => {
+        glue.loadLearningObjectSummary(req.body.id)
+            .then((objects)=>{ res.send(objects); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/loadLearningObject', (req, res) => {
+        glue.loadLearningObject(req.body.id)
+            .then((object)=>{ res.send(object); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/addUser', (req, res) => {
+        glue.addUser(req.body.user)
+            .then((id)=>{ res.send(id); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/addLearningObject', (req, res) => {
+        glue.addLearningObject(req.body.author, req.body.object)
+            .then((id)=>{ res.send(id); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/addLearningOutcome', (req, res) => {
+        glue.addLearningOutcome(req.body.source, req.body.outcome)
+            .then((id)=>{ res.send(id); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/editUser', (req, res) => {
+        glue.editUser(req.body.id, req.body.user)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/editLearningObject', (req, res) => {
+        glue.editLearningObject(req.body.id, req.body.object)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/editLearningOutcome', (req, res) => {
+        glue.editLearningOutcome(req.body.id, req.body.outcome)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/updateLearningObject', (req, res) => {
+        glue.updateLearningObject(req.body.id, req.body.object)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+    
+    server.get('/reorderObject', (req, res) => {
+        db.reorderObject(req.body.user, req.body.object, req.body.index)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/reorderOutcome', (req, res) => {
+        db.reorderOutcome(req.body.object, req.body.outcome, req.body.index)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/unmapOutcome', (req, res) => {
+        db.unmapOutcome(req.body.outcome, req.body.mapping)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/mapOutcome', (req, res) => {
+        db.mapOutcome(req.body.outcome, req.body.mapping)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/deleteUser', (req, res) => {
+        db.deleteUser(req.body.id)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/deleteLearningObject', (req, res) => {
+        db.deleteLearningObject(req.body.id)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.get('/deleteLearningOutcome', (req, res) => {
+        db.deleteLearningOutcome(req.body.id)
+            .then(()=>{ res.send(); })
+            .catch((err)=>{ res.send({error:err}); });
+    });
+
+    server.listen(process.env["CLARK_DB_INTERACTOR_PORT"]);
+    console.log("Listening on port "+process.env["CLARK_DB_INTERACTOR_PORT"]);
   })
   .catch((err) => {
     console.log(err);
