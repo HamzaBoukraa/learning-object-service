3643544eeef96a9dd01e84167ba84b2835afe45b
Kyle Sherbert <ksherb1@students.towson.edu>
2018-01-05 16:24:02 -0500

    Refine setup script to migrate user-created data
    Resolves #10

--- db-setup/db-setup.script.ts
+++ db-setup/db-setup.script.ts
@@ -4,47 +4,250 @@
 // tslint:disable-next-line: no-require-imports
 require('../useme');
 
-import { MongoClient } from 'mongodb';
+import * as lineReader from 'line-reader';
+
+import { MongoClient, Db } from 'mongodb';
 
 import { DBInterface, HashInterface } from '../interfaces/interfaces';
 import { MongoDriver, BcryptDriver } from '../drivers/drivers';
 
 import { DBGluer } from '../db.gluer';
+import { StandardOutcome } from '../entity/entities';
+import {
+    collections, schemaFor, collectionFor, uniquesFor, textsFor,
+} from '../schema/db.schema';
+import {
+    UserSchema, LearningObjectSchema, LearningOutcomeSchema,
+} from '../schema/schema';
 
-import { init } from './db-init.script';
-import { NCWF } from './db-NCWF.script';
-import { NCWFtasks } from './db-NCWF-tasks.script';
-import { CAE } from './db-CAE.script';
-import { CS2013 } from './db-CS2013.script';
-import { fill } from './db-fill.script';
+/**
+ * Initialize database with collections and indexes as defined by schema
+ * NOTE: this destroys all data already in the database
+ *
+ * @param {Db} prod mongo Db object connected to db being initialized
+ */
+export async function init(db: Db) {
+    // drop collections
+    for (let collection of collections()) {
+        try {
+            await db.dropCollection(collection);
+            console.log('Deleted existing collection: ' + collection);
+        } catch (e) {/* an error here simply means there was no collection to drop*/ }
+    }
+
+    // add collections
+    try {
+        for (let collection of collections()) {
+
+            let c = await db.createCollection(collection);
+            console.log('Created collection ' + collection);
+
+            let uniques = uniquesFor(schemaFor(collection));
+            let texts = textsFor(schemaFor(collection));
+
+            // index creation is slightly different if both have the same properties
+            if (uniques && texts && uniques.equals(texts)) {
+                let props = {};
+                for (let text of texts) {
+                    props[text] = 'text';
+                }
+                await c.createIndex(props, { unique: true });
+                console.log('Created joint unique-text index on ' + texts.toString() + ' for ' + collection + ' collection');
+            } else {
+                if (uniques) {
+                    let props = {};
+                    for (let unique of uniques) {
+                        props[unique] = 1;
+                    }
+                    await c.createIndex(props, { unique: true });
+                    console.log('Created unique index on ' + uniques.toString() + ' for ' + collection + ' collection');
+                }
+                // create text index
+                if (texts) {
+                    let props = {};
+                    for (let text of texts) {
+                        props[text] = 'text';
+                    }
+                    await c.createIndex(props);
+                    console.log('Created text index on ' + texts.toString() + ' for ' + collection + ' collection');
+                }
+            }
+        }
+
+        return Promise.resolve();
+    } catch (e) {
+        return Promise.reject('Initialization failed: ' + e);
+    }
+}
+
+/**
+ * Add standard outcomes from data file
+ *
+ * Data file has two columns: [ name | outcome ] separated by tab
+ *
+ * @param {string} author author-label for all outcomes in the file
+ * @param {string} date date-label for all outcomes in the file
+ * @param {string} file file path/name of data file
+ * @param {DBGluer} glue gluer connected to db being filled
+ */
+export async function addStandards(author: string, date: string, file: string, glue: DBGluer) {
+    return new Promise<void>((resolve, reject) => {
+        let promises: Promise<any>[] = [];
+        let cnt = 0;    // count successful adds
+
+        // what to do for each record
+        lineReader.eachLine(file, function (line, last) {
+            let dat = line.split('\t');
+            if (dat.length === 2) {
+                let outcome = new StandardOutcome(author, dat[0], date, dat[1]);
+                promises.push(
+                    glue.addStandardOutcome(outcome)
+                        .then(() => {
+                            cnt += 1;
+                            return Promise.resolve();
+                        })
+                        .catch((err) => { console.log(author + 'insertion error: ' + err); }),
+                );
+            } else {    // data formatting error
+                console.log('Could not process line: ' + line);
+            }
+
+            // if we just processed the last line, wait to resolve all promises
+            if (last) {
+                Promise.all(promises)
+                       .then(() => {
+                            console.log(cnt + ' ' + author + ' standard outcomes added');
+                            resolve();
+                        })
+                       .catch((e) => { reject(e); });
+            }
+        });
+    });
+}
+
+async function copyCollection(collection: string, tmp: Db, prod: Db) {
+    return new Promise<void>((resolve, reject) => {
+        let promises: Promise<any>[] = [];
+
+        let docs = tmp.collection(collection).find();
+        docs.forEach(
+            (doc) => {
+                promises.push(prod.collection(collection).insertOne(doc));
+            },
+            (err) => {
+                if (err) reject(err);
+                else Promise.all(promises)
+                            .then(() => { resolve(); })
+                            .catch((e) => { reject(e); });
+            },
+        );
+    });
+}
+
+async function copyOutcome(outcome: any, tmp: Db, prod: Db, glue: DBGluer) {
+    try {
+        let collection = collectionFor(LearningOutcomeSchema);
+        if ( !outcome.mappings ) return Promise.resolve();    // skip standard outcomes
+
+        let index = 0;
+        while (index < outcome.mappings.length) {
+            let mapping = await tmp.collection(collection)
+                                .findOne({ _id: outcome.mappings[index] });
+            if ( !outcome.mappings ) {   // now skip non-standard outcomes
+                let analog = await prod.collection(collection)
+                                    .findOne({ tag: mapping.tag });
+
+                // if mapped standard outcome is not in production, splice it out
+                if ( !analog ) outcome.mappings.splice(index, 1);   // length decrements
+                // otherwise, replace with the new objectid
+                else {
+                    outcome.mappings[index] = analog._id;
+                    index += 1;                                    // index increments
+                }
+            }
+        }
+
+        await prod.collection(collection).insertOne(outcome);
+        return Promise.resolve();
+    } catch (err) {
+        return Promise.reject(err);
+    }
+}
+
+async function copyOutcomes(tmp: Db, prod: Db, glue: DBGluer) {
+    let collection = collectionFor(LearningOutcomeSchema);
+    return new Promise<void>((resolve, reject) => {
+        let outcomes = tmp.collection(collection).find();
+        let promises: Promise<void>[] = [];
+        outcomes.forEach(
+            (doc) => {
+                promises.push(copyOutcome(doc, tmp, prod, glue));
+            },
+            (err) => {
+                if (err) reject(err);
+                else Promise.all(promises)
+                            .then(() => { resolve(); })
+                            .catch((e) => { reject(e); });
+            });
+    });
+}
+
+/**
+ * Merge user-created data into production server.
+ * NOTE: at present this function is only meant to act on
+ *       an initialized database but empty save for standard outcomes
+ *       That is, there is no safeguarding against
+ *          duplicate user-created data.
+ *
+ * @param {Db} tmp mongo Db object connected to temporary dump db for live data
+ * @param {Db} prod mongo Db object connected to db being filled
+ * @param {DBGluer} glue gluer connected to db being filled
+ */
+export async function merge(tmp: Db, prod: Db, glue: DBGluer) {
+    return Promise.all([
+            copyCollection(collectionFor(UserSchema), tmp, prod),
+            copyCollection(collectionFor(LearningObjectSchema), tmp, prod),
+            copyOutcomes(tmp, prod, glue),
+        ]);
+}
+
 
 // run initialization script
+if (require.main === module) {
+    console.log('--- Initializing ---');
+    MongoClient.connect(process.env.CLARK_DB_URI, async (err, dbase) => {
+        try {
+            if (err) throw err;
 
-console.log('--- Initializing ---');
-MongoClient.connect(process.env.CLARK_DB_URI, async (err, dbase) => {
-  if (err) throw err;
-  else {
-    await init(dbase);
-    dbase.close();
+            await init(dbase);
+            dbase.close();
 
-    let db: DBInterface = new MongoDriver();
-    let hash: HashInterface = new BcryptDriver(10);
-    let glue = new DBGluer(db, hash);
+            let db: DBInterface = new MongoDriver();
+            let hash: HashInterface = new BcryptDriver(10);
+            let glue = new DBGluer(db, hash);
 
-    db.connect(process.env.CLARK_DB_URI)
-      .then(() => {
-        console.log('--- Adding Standard Outcomes ---');
-        NCWF(glue).catch((e) => { console.log('Failed to add NCWF outcomes: ' + e); });
-        NCWFtasks(glue).catch((e) => { console.log('Failed to add NCWF tasks: ' + e); });
-        CAE(glue).catch((e) => { console.log('Failed to add CAE outcomes: ' + e); });
-        CS2013(glue).catch((e) => { console.log('Failed to add CS2013 outcomes: ' + e); });
+            await db.connect(process.env.CLARK_DB_URI);
+            console.log('--- Adding Standard Outcomes ---');
+            await Promise.all([
+                addStandards('NCWF', '2017', 'dbcontent/NIST.SP.800-181.dat', glue),
+                addStandards('NCWF Tasks', '2017', 'dbcontent/NIST.SP.800-181.tasks.dat', glue),
+                addStandards('CAE', '2014', 'dbcontent/CAE-CD_Knowledge_Units.dat', glue),
+                addStandards('CS 2013', '2013', 'dbcontent/CS_2013_Learning_Outcomes.dat', glue),
+            ]);
+            db.disconnect();
 
-        console.log('--- Adding Legacy Objects ---');
-        fill(glue).catch((e) => { console.log('Failed to add legacies: ' + e); });
-      }).catch((e) => {
-        console.log('Failed to connect: ' + e);
-      });
+            // if env variable 'SKIP_MERGE' exists, do not merge from tmp db
+            if (!process.env.SKIP_MERGE) {
+                let production = await MongoClient.connect(process.env.CLARK_DB_URI);
+                let backup = await MongoClient.connect(process.env.CLARK_DB_URI.replace('onion', 'tmp'));
+                console.log('--- Merging previous data ---');
+                await merge(backup, production, glue);
 
-    setTimeout(db.disconnect, 3000);
-  }
-});
+                production.close();
+                backup.close();
+            }
+        } catch (e) {
+            console.log('Setup incomplete:' + e);
+        }
+    });
+}
---
+++ db-setup_portal.sh
@@ -0,0 +1,62 @@
+REPLICA=ClarkDB-shard-0
+LIVE=clarkdb-shard-00-00-rktle.mongodb.net:27017,clarkdb-shard-00-01-rktle.mongodb.net:27017,clarkdb-shard-00-02-rktle.mongodb.net:27017
+USER=secinj
+# PASS must be given by user
+DB=onion
+
+# juggle $HOST variable for intuitive use later on
+if [ "$HOST" = "atlas" ]    # if HOST is set to 'atlas', flag script to reset live db
+then
+    unset HOST
+    echo "*** Resetting live db ***"
+else
+    if [ ! "$HOST" ]       # if HOST isn't set, set it to the default localhost
+    then
+        HOST=localhost:27017
+    fi
+    echo "*** Setting up db on $HOST ***"
+fi
+
+if [ ! "$PASS" ]
+then
+    # all we're doing is running setup without merging
+    if [ "$HOST" ]
+    then # setting up database on private machine
+        echo "*** Running setup script (no backup) ***"
+        SKIP_MERGE=1 CLARK_DB_URI="mongodb://$HOST/$DB" node dist/db-setup/db-restart.script.js
+    else
+        echo "Please prefix script with 'PASS=<atlas-pwd> '"
+    fi
+else
+    echo "*** Backing up live db ***"
+    mongodump --host "$REPLICA/$LIVE" --ssl --username $USER --password $PASS --authenticationDatabase admin --db $DB --out tmp/dump && \
+    mv tmp/dump/$DB tmp/dump/tmp
+    ls tmp
+
+    echo "*** Restoring live db to tmp database ***"
+    if [ "$HOST" ]
+    then # setting up database on private machine
+        mongo --host $HOST tmp --eval "db.dropDatabase()"
+        mongorestore --host $HOST tmp/dump
+    else                # otherwise, restarting live database
+        mongo "mongodb://$LIVE/tmp?replicaSet=$REPLICA" --ssl --username  $USER --password $PASS --authenticationDatabase admin --eval "db.dropDatabase()"
+        mongorestore --host "$REPLICA/$LIVE" --ssl --username $USER --password $PASS --authenticationDatabase admin --dir tmp/dump
+    fi
+
+    echo "*** Running setup script ***"
+    if [ "$HOST" ]
+    then # setting up database on private machine
+        CLARK_DB_URI="mongodb://$HOST/$DB" node dist/db-setup/db-restart.script.js
+    else
+        CLARK_DB_URI="mongodb://$USER:$PASS@$LIVE/$DB?ssl=true&replicaSet=$REPLICA&authSource=admin" node dist/db-setup/db-restart.script.js
+    fi
+
+    echo "*** Cleaning up temporary data ***"
+    if [ "$HOST" ]
+    then # setting up database on private machine
+        mongo --host $HOST tmp --eval "db.dropDatabase()"
+    else
+        mongo "mongodb://$LIVE/tmp?replicaSet=$REPLICA" --ssl --username  $USER --password $PASS --authenticationDatabase admin --eval "db.dropDatabase()"
+    fi
+    rm -r tmp
+fi
