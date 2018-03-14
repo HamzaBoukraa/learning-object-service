e9d9854fbd48093e4e8b024854da8dc78505b20f
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-14 10:53:46 -0400

    Change API output text

--- src/drivers/express/ExpressDriver.ts
+++ src/drivers/express/ExpressDriver.ts
@@ -45,7 +45,9 @@ export class ExpressDriver {
     /**
      * Listen on provided port, on all network interfaces.
      */
-    server.listen(port, () => console.log(`API running on localhost:${port}`));
+    server.listen(port, () =>
+      console.log(`Learning Object Service running on localhost:${port}`)
+    );
 
     return this.app;
   }
