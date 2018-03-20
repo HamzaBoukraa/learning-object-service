a1fcc0129186ced1d6edc1ae20b7ee521637b7f5
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-20 09:07:44 -0400

    Clean up responder interface

--- src/interfaces/Responder.ts
+++ src/interfaces/Responder.ts
@@ -1,6 +1,5 @@
 export interface Responder {
-    sendOperationSuccess(): void;
-    sendOperationError(error?: string, status?: number): void;
-    sendOperationErrorJSON(error?: { error: string }, status?: number): void;
-    sendObject(object: any): void;
+  sendOperationSuccess(): void;
+  sendOperationError(error?: string, status?: number): void;
+  sendObject(object: any): void;
 }
