1e4ac9531d09742ef866f44dd7c9b1c86e622c33
Gustavus Shaw II <gshaw1997@gmail.com>
2018-06-04 13:04:00 -0400

    Fix bug in admin middleware

--- src/middleware/admin-access.ts
+++ src/middleware/admin-access.ts
@@ -1,9 +1,12 @@
+import * as jwt from 'jsonwebtoken';
+
 export function enforceAdminAccess(req, res, next) {
-  if (
-    req.user &&
-    req.user.accessGroups &&
-    req.user.accessGroups.includes('admin')
-  ) {
+  let user = req.user;
+  const cookie = req.cookies.presence;
+  if (!user && cookie) {
+    user = jwt.decode(cookie);
+  }
+  if (user && user.accessGroups && user.accessGroups.includes('admin')) {
     next();
   } else {
     res.status(401).send('Invalid access!');
