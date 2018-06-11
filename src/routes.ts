cb7aea5442e97bfa5ccbe9a778af56babd2eb84d
Gustavus Shaw II <gshaw1997@gmail.com>
2018-06-11 16:07:18 -0400

    Update Clark Entity

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const CART_ROUTES = {
   METRICS(objectID: string) {
     return `${process.env.CART_API}/learning-objects/${objectID}/metrics`;
