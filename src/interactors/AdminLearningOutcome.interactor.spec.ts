59c1266bd4d1a93e4af1042b5d468c4200c3f1d7
Nicholas Visalli <nvisal1225@gmail.com>
2018-06-15 14:19:40 -0400

    more unit tests for learning object interactor

--- src/interactors/AdminLearningOutcome.interactor.spec.ts
+++ src/interactors/AdminLearningOutcome.interactor.spec.ts
@@ -1,98 +1,98 @@
-import { LearningObjectInteractor, AdminLearningObjectInteractor } from '../interactors/interactors';
-import  MongoDriver from '../drivers/MongoDriver';
-// import { expect } from 'chai';
-// import RouteResponder  from '../drivers/RouteResponder';
-// import { expect } from 'chai';
-const driver = new MongoDriver; // DataStore
-// const responder = new RouteResponder; // Responder
-const dburi = process.env.CLARK_DB_URI_DEV.replace(
-    /<DB_PASSWORD>/g,
-    process.env.CLARK_DB_PWD,
-  )
-  .replace(/<DB_PORT>/g, process.env.CLARK_DB_PORT)
-  .replace(/<DB_NAME>/g, process.env.CLARK_DB_NAME);
+// import { LearningObjectInteractor, AdminLearningObjectInteractor } from '../interactors/interactors';
+// import  MongoDriver from '../drivers/MongoDriver';
+// // import { expect } from 'chai';
+// // import RouteResponder  from '../drivers/RouteResponder';
+// // import { expect } from 'chai';
+// const driver = new MongoDriver; // DataStore
+// // const responder = new RouteResponder; // Responder
+// const dburi = process.env.CLARK_DB_URI_DEV.replace(
+//     /<DB_PASSWORD>/g,
+//     process.env.CLARK_DB_PWD,
+//   )
+//   .replace(/<DB_PORT>/g, process.env.CLARK_DB_PORT)
+//   .replace(/<DB_NAME>/g, process.env.CLARK_DB_NAME);
 
-// ** fetchAllObjects **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
-describe('fetchAllObjects', () => {
-    // Test 1: Provide expected input
-  it('should return an array of objects', done => {
-    driver.connect(dburi).then(val => {
-      return AdminLearningObjectInteractor.fetchAllObjects(driver).then(val => {
-        expect(val).toBeTruthy();
-        done();
-      }).catch((error) => {
-        console.log(error);
-        expect(true).toBe(false);
-        done();
-      });
-    }).catch((error) => {
-      console.log(error);
-      expect(true).toBe(false);
-      done();
-    });
-  });
-   // Test 2: Provide expected input
-  it('should return an error - given invalid data store!', done => {
-    driver.connect(dburi).then(val => {
-      return AdminLearningObjectInteractor.fetchAllObjects(this.driver).then(val => {
-        expect(true).toBe(false);
-        done();
-      }).catch((error) => {
-        expect(error).toBeTruthy();
-        done();
-      });
-    }).catch((error) => {
-      expect(true).toBe(false);
-      done();
-    });
-  });
-});
-
-// describe('searchObjects', () => {
-//     // Test 1: Provide expected input 
-//   it('should return an array of users', done => {
-//     driver.connect(dburi).then(val => {
-//       const query = { username: 'nvisal1' };
-//       return AdminLearningObjectInteractor.searchObjects(driver, 'text').then(val => {
-//         console.log(val);
-//         expect(val).toBeTruthy();
-//         done();
-//       }).catch((error) => {
-//         console.log(error);
-//         done();
-//       });
-//     }).catch((error) => {
-//       console.log(error);
-//       done();
-//     });
-//   });
-// });
-
-// ** togglePublished **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
-// describe('togglePublished', () => {
+// // ** fetchAllObjects **
+// // params (dataStore: DataStore, currPage?: number, limit?: number)
+// //   organization?: string;
+// // success - returns an array of users
+// // failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
+// describe('fetchAllObjects', () => {
 //     // Test 1: Provide expected input
 //   it('should return an array of objects', done => {
 //     driver.connect(dburi).then(val => {
-//       return AdminLearningObjectInteractor.togglePublished(driver, username, id, published).then(val => {
-//         // expect(response.error).toEqual('Server error encounter.');
-//         console.log(val);
+//       return AdminLearningObjectInteractor.fetchAllObjects(driver).then(val => {
 //         expect(val).toBeTruthy();
 //         done();
 //       }).catch((error) => {
 //         console.log(error);
+//         expect(true).toBe(false);
 //         done();
 //       });
 //     }).catch((error) => {
 //       console.log(error);
+//       expect(true).toBe(false);
+//       done();
+//     });
+//   });
+//    // Test 2: Provide expected input
+//   it('should return an error - given invalid data store!', done => {
+//     driver.connect(dburi).then(val => {
+//       return AdminLearningObjectInteractor.fetchAllObjects(this.driver).then(val => {
+//         expect(true).toBe(false);
+//         done();
+//       }).catch((error) => {
+//         expect(error).toBeTruthy();
+//         done();
+//       });
+//     }).catch((error) => {
+//       expect(true).toBe(false);
 //       done();
 //     });
 //   });
 // });
+
+// // describe('searchObjects', () => {
+// //     // Test 1: Provide expected input 
+// //   it('should return an array of users', done => {
+// //     driver.connect(dburi).then(val => {
+// //       const query = { username: 'nvisal1' };
+// //       return AdminLearningObjectInteractor.searchObjects(driver, 'text').then(val => {
+// //         console.log(val);
+// //         expect(val).toBeTruthy();
+// //         done();
+// //       }).catch((error) => {
+// //         console.log(error);
+// //         done();
+// //       });
+// //     }).catch((error) => {
+// //       console.log(error);
+// //       done();
+// //     });
+// //   });
+// // });
+
+// // ** togglePublished **
+// // params (dataStore: DataStore, currPage?: number, limit?: number)
+// //   organization?: string;
+// // success - returns an array of users
+// // failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
+// // describe('togglePublished', () => {
+// //     // Test 1: Provide expected input
+// //   it('should return an array of objects', done => {
+// //     driver.connect(dburi).then(val => {
+// //       return AdminLearningObjectInteractor.togglePublished(driver, username, id, published).then(val => {
+// //         // expect(response.error).toEqual('Server error encounter.');
+// //         console.log(val);
+// //         expect(val).toBeTruthy();
+// //         done();
+// //       }).catch((error) => {
+// //         console.log(error);
+// //         done();
+// //       });
+// //     }).catch((error) => {
+// //       console.log(error);
+// //       done();
+// //     });
+// //   });
+// // });
--- src/interactors/LearningObject.interactor.spec.ts
+++ src/interactors/LearningObject.interactor.spec.ts
@@ -1,176 +1,115 @@
 import { LearningObjectInteractor, AdminLearningObjectInteractor } from '../interactors/interactors';
 import  MongoDriver from '../drivers/MongoDriver';
-// import { expect } from 'chai';
-// import RouteResponder  from '../drivers/RouteResponder';
-// import { expect } from 'chai';
+// const expect = require('chai').expect;
+import { expect } from 'chai';
 const driver = new MongoDriver; // DataStore
-// const responder = new RouteResponder; // Responder
-const dburi = process.env.CLARK_DB_URI_DEV.replace(
+
+beforeAll(done => {
+  // Before running any tests, connect to database
+  const dburi = process.env.CLARK_DB_URI_DEV.replace(
     /<DB_PASSWORD>/g,
     process.env.CLARK_DB_PWD,
   )
   .replace(/<DB_PORT>/g, process.env.CLARK_DB_PORT)
   .replace(/<DB_NAME>/g, process.env.CLARK_DB_NAME);
+  driver.connect(dburi).then(val => {
+    console.log('connected to database');
+    done();
+  }).catch((error) => {
+    console.log('failed to connect to database');
+    done();
+  });
+});
 
-// ** loadLearningObjectSummary **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('loadLearningObjectSummary', () => {
     // Test 1: Provide expected input
   it('should load learning object summary', done => {
-    driver.connect(dburi).then(val => {
       const username = 'nvisal1';
       return LearningObjectInteractor.loadLearningObjectSummary(driver, username ).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('array');
         done();
       }).catch((error) => {
-        expect(true).toBe(false);
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      expect(true).toBe(false);
-      done();
-    });
   });
     // Test 2: Provide expected input
   it('should return error - empty username given!', done => {
-        driver.connect(dburi).then(val => {
           const username = '';
           return LearningObjectInteractor.loadLearningObjectSummary(driver, username ).then(val => {
-            // expect(response.error).toEqual('Server error encounter.');
-            expect(true).toBe(false);
+            expect.fail();
             done();
           }).catch((error) => {
-            expect(error).toBeTruthy();
+            expect(error).to.be.a('string');
             done();
           });
-        }).catch((error) => {
-          expect(true).toBe(false);
-          done();
-        });
       });
 });
 
-// ** loadLearningObject **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('loadLearningObject', () => {
     // Test 1: Provide expected input
-  it('should load learning object - invalid access - throws error!', done => {
-    driver.connect(dburi).then(val => {
+  it('should load learning object', done => {
       const username = 'nvisal1';
-      const learningObjectName = 'test contributors';
+      const learningObjectName = 'testing more contributors 2';
       return LearningObjectInteractor.loadLearningObject(driver, username, learningObjectName).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(true).toBe(false);
+        expect(val).to.be.an('object');
         done();
       }).catch((error) => {
-        console.log(error);
-        expect(error).toBeTruthy();
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      expect(true).toBe(false);
-      done();
-    });
   });
     // Test 2: Provide expected input
   it('should return error - incorrect user', done => {
-        driver.connect(dburi).then(val => {
           const username = '';
-          const learningObjectName = 'test contributors';
+          const learningObjectName = 'testing more contributors 2';
           return LearningObjectInteractor.loadLearningObject(driver, username, learningObjectName).then(val => {
-            // expect(response.error).toEqual('Server error encounter.');
-            expect(true).toBe(false);
+            expect.fail();
             done();
           }).catch((error) => {
-            console.log(error);
-            expect(error).toBeTruthy();
+            expect(error).to.be.a('string');
             done();
           });
-        }).catch((error) => {
-          console.log(error);
-          expect(true).toBe(false);
-          done();
-        });
       });
 });
 
-// ** loadFullLearningObjectByIDs **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('loadFullLearningObjectByIDs', () => {
     // Test 1: Provide expected input
   it('should load full learning object', done => {
-    driver.connect(dburi).then(val => {
       const ids = ['5b17ea3be3a1c4761f7f9463', '5b17ea3be3a1c4761f7f9463'];
       return LearningObjectInteractor.loadFullLearningObjectByIDs(driver, ids ).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('array');
         done();
       }).catch((error) => {
-        console.log(error);
-        expect(true).toBe(false);
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      expect(true).toBe(false);
-      done();
-    });
   });
     // Test 1: Provide expected input
   it('should return learning object - given empty array!', done => {
-        driver.connect(dburi).then(val => {
           const ids = [''];
           return LearningObjectInteractor.loadFullLearningObjectByIDs(driver, ids ).then(val => {
-            // expect(response.error).toEqual('Server error encounter.');
-            expect(val).toBeTruthy();
+            expect(val).to.be.an('array');
             done();
           }).catch((error) => {
-            console.log(error);
-            expect(true).toBe(false);
+            expect.fail();
             done();
           });
-        }).catch((error) => {
-          console.log(error);
-          expect(true).toBe(false);
-          done();
-        });
       });
 });
 
-// ** findLearningObject **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('findLearningObject', () => {
     // Test 1: Provide expected input
-  it('should find a learning object', done => {
-    driver.connect(dburi).then(val => {
+  it('should find a learning object ID', done => {
       const username = 'nvisal1';
-      const learningObjectName = 'test contributors';
+      const learningObjectName = 'testing more contributors 2';
       return LearningObjectInteractor.findLearningObject(driver, username, learningObjectName ).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.a('string');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      done();
-    });
   });
 });
 
@@ -202,155 +141,125 @@ describe('findLearningObject', () => {
 //   });
 // });
 
-// ** togglePublished **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('togglePublished', () => {
     // Test 1: Provide expected input
-  it('should toggle published status', done => {
-    driver.connect(dburi).then(val => {
+  it('should return error', done => {
       const username = 'nvisal1';
-      const learningObjectName = 'test contributors';
-      const id = '5b17ea3be3a1c4761f7f9463';
+      const learningObjectName = 'testing more contributors 2';
+      const id = '';
       const published = true;
       return LearningObjectInteractor.togglePublished(driver, username, id, published).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect.fail();
         done();
       }).catch((error) => {
-        console.log(error);
+        expect(error).to.be.a('string');
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      done();
-    });
   });
 });
 
-// ** fetchAllObjects **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('fetchAllObjects', () => {
     // Test 1: Provide expected input
   it('should fetch all objects', done => {
-    driver.connect(dburi).then(val => {
       const currPage = 1;
       const limit = 3;
       return LearningObjectInteractor.fetchAllObjects(driver, currPage, limit).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('object');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      done();
-    });
   });
 });
 
-// ** fetchMultipleObjects **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('fetchMultipleObjects', () => {
     // Test 1: Provide expected input
-  it('should fetch multiple learning objects', done => {
-    driver.connect(dburi).then(val => {
-      const ids = [{username: 'nvisal1', learningObjectName: 'test contributors'}];
+  it('should return an array of objects - based on given username and lo name', done => {
+      const ids = [{username: 'nvisal1', learningObjectName: 'testing more contributors 2'}];
       return LearningObjectInteractor.fetchMultipleObjects(driver, ids).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('array');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
-    }).catch((error) => {
-      console.log(error);
-      done();
-    });
   });
 });
 
-// ** fetchObjectsByIDs **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('fetchObjectsByIDs', () => {
-    // Test 1: Provide expected input
-  it('should fetch multiple learning objects', done => {
-    driver.connect(dburi).then(val => {
+  it('should return an array of objects - based on given IDs', done => {
       const ids = ['5b17ea3be3a1c4761f7f9463'];
       return LearningObjectInteractor.fetchObjectsByIDs(driver, ids).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('array');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
+  });
+  it('should return an error - invalid data store provided!', done => {
+    const ids = ['5b17ea3be3a1c4761f7f9463'];
+    return LearningObjectInteractor.fetchObjectsByIDs(this.driver, ids).then(val => {
+      expect.fail();
+      done();
     }).catch((error) => {
-      console.log(error);
+      expect(error).to.be.a('string');
       done();
     });
   });
 });
 
-// ** fetchCollections **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('fetchCollections', () => {
-    // Test 1: Provide expected input
-  it('should fetch multiple learning objects', done => {
-    driver.connect(dburi).then(val => {
+  it('should return an array of objects - these objects contain lo IDs', done => {
       return LearningObjectInteractor.fetchCollections(driver).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+        expect(val).to.be.an('array');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
+  });
+  it('should return an error - invalid data store provided!', done => {
+    return LearningObjectInteractor.fetchCollections(this.driver).then(val => {
+      expect.fail();
+      done();
     }).catch((error) => {
-      console.log(error);
+      expect(error).to.be.a('string');
       done();
     });
-  });
+});
 });
 
-// ** fetchCollection **
-// params (dataStore: DataStore, currPage?: number, limit?: number)
-//   organization?: string;
-// success - returns an array of users
-// failure - returns Promise.reject(`Problem searching users. Error: ${e}`);
 describe('fetchCollection', () => {
     // Test 1: Provide expected input
-  it('should fetch multiple learning objects', done => {
-    driver.connect(dburi).then(val => {
-      const name = 'test contributors';
-      return LearningObjectInteractor.fetchCollection(driver, name).then(val => {
-        // expect(response.error).toEqual('Server error encounter.');
-        expect(val).toBeTruthy();
+  it('should return an object - contains an array of learning objects - *** above logs caused from this test ***', done => {
+      // Extra time for this test is required, will timeout otherwise!
+      jest.setTimeout(10000);
+      const collectionName = 'NSA NCCP';
+      return LearningObjectInteractor.fetchCollection(driver, collectionName).then(val => {
+        expect(val).to.be.an('object');
         done();
       }).catch((error) => {
-        console.log(error);
+        expect.fail();
         done();
       });
+  });
+  it('should return an error - invalid collection name', done => {
+    const collectionName = '';
+    return LearningObjectInteractor.fetchCollection(driver, collectionName).then(val => {
+      expect.fail();
+      done();
     }).catch((error) => {
-      console.log(error);
+      expect(error).to.be.a('string');
       done();
     });
   });
 });
+
+afterAll (() => {
+    driver.disconnect();
+    console.log('Disconnected from database');
+});
+
+
