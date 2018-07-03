9ac6a63de4da343cb3efa0c724762ece39dc6a1e
Gustavus Shaw II <gshaw1997@gmail.com>
2018-07-03 15:00:56 -0400

    Export ExpressAuthRouteDriver from drivers

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -1,5 +1,6 @@
 import { ExpressDriver } from './express/ExpressDriver';
 import { ExpressRouteDriver } from './express/ExpressRouteDriver';
+import { ExpressAuthRouteDriver } from './express/ExpressAuthRouteDriver';
 import { ExpressAdminRouteDriver } from './express/ExpressAdminRouteDriver';
 import { ExpressResponder } from './express/ExpressResponder';
 import { MongoDriver } from './MongoDriver';
@@ -7,6 +8,7 @@ import { S3Driver } from './S3Driver';
 export {
   ExpressDriver,
   ExpressRouteDriver,
+  ExpressAuthRouteDriver,
   ExpressAdminRouteDriver,
   ExpressResponder,
   MongoDriver,
