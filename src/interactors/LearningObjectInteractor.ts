1a49eac7fc1240343572585896bfed6a218e8fbf
Gustavus Shaw II <gshaw1997@gmail.com>
2018-08-12 18:27:49 -0400

    Patch file upload

--- src/interactors/LearningObjectInteractor.ts
+++ src/interactors/LearningObjectInteractor.ts
@@ -297,7 +297,10 @@ export class LearningObjectInteractor {
             filePathMap,
             loFile,
           )}`;
-          loFile.url = await fileManager.upload(path, file);
+          loFile.url = await fileManager.upload(
+            path,
+            file.buffer.length ? file.buffer : Buffer.from(file.buffer),
+          );
           if (parent) {
             loFile.fullPath = path;
           }
