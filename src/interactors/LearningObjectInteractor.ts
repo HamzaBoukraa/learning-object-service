a5f6093bec2efc8f1ffc1238ac93b5cbec5fb775
Sean Donnelly <sdonnelly3000@gmail.com>
2018-08-14 12:58:09 -0400

    Return the id of any requested learning object

--- src/interactors/LearningObjectInteractor.ts
+++ src/interactors/LearningObjectInteractor.ts
@@ -125,9 +125,7 @@ export class LearningObjectInteractor {
         true,
         accessUnpublished,
       );
-      if (accessUnpublished) {
-        learningObject.id = learningObjectID;
-      }
+      learningObject.id = learningObjectID;
 
       if (learningObject.children) {
         learningObject.children = await this.loadChildObjects(
@@ -143,7 +141,6 @@ export class LearningObjectInteractor {
       } catch (e) {
         console.log(e);
       }
-
       return learningObject;
     } catch (e) {
       return Promise.reject(e);
