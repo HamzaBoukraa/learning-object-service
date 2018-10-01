a696e43c1ae971fb80ba5bcc915ef381acbc0c6e
Sean Donnelly <sdonnelly3000@gmail.com>
2018-10-01 12:15:00 -0400

    :fire: Remove spy pattern from Responder
    The spy pattern was introduced to allow for chaining of interactor methods, where only the first in the call chain knew the scope of the operation. This pattern has not been found to be useful with other changes, and is therefore not being used anymore.

--- src/drivers/express/ExpressResponder.ts
+++ src/drivers/express/ExpressResponder.ts
@@ -1,15 +1,9 @@
 import { Responder } from '../../interfaces/interfaces';
 import { Response } from 'express';
-import { Subject } from 'rxjs';
-import { LearningObject } from '@cyber4all/clark-entity';
 
 export class ExpressResponder implements Responder {
-  spy: Subject<any>;
-  shouldReturn: boolean;
 
-  constructor(private res: Response) {
-    this.spy = new Subject<any>();
-  }
+  constructor(private res: Response) { }
   sendOperationSuccess(): void {
     this.res.sendStatus(200);
   }
@@ -17,18 +11,10 @@ export class ExpressResponder implements Responder {
     error: string = 'Server error encounter.',
     status: number = 400,
   ): void {
-    if (this.shouldReturn) {
-      this.spy.next({ error, status });
-    } else {
-      this.res.status(status).send(error);
-    }
+    this.res.status(status).send(error);
   }
   sendObject(object: any): void {
-    if (this.shouldReturn) {
-      this.spy.next(object);
-    } else {
-      this.res.status(200).send(object);
-    }
+    this.res.status(200).send(object);
   }
 
   unauthorized(message?: string) {
--- src/interfaces/Responder.ts
+++ src/interfaces/Responder.ts
@@ -1,9 +1,6 @@
-import { Subject } from 'rxjs';
 import { Response } from 'express';
 
 export interface Responder {
-  spy: Subject<any>;
-  shouldReturn: boolean;
   sendOperationSuccess(): void;
   sendOperationError(error?: string, status?: number): void;
   sendObject(object: any): void;
