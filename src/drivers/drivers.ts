9a355a3960a872b606a45a9899d325b8d5554691
Sean Donnelly <sdonnelly3000@gmail.com>
2018-10-10 12:19:06 -0400

    :truck: Move S3 modules to FileManager

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -4,7 +4,7 @@ import { ExpressAuthRouteDriver } from './express/ExpressAuthRouteDriver';
 import { ExpressAdminRouteDriver } from './express/ExpressAdminRouteDriver';
 import { ExpressResponder } from './express/ExpressResponder';
 import { MongoDriver } from './MongoDriver';
-import { S3Driver } from './S3Driver';
+import { S3Driver } from '../FileManager/S3Driver';
 export {
   ExpressDriver,
   ExpressRouteDriver,
