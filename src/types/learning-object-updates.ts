033737e7b5abfa2ed6975c38fc790755ffff5c07
Gustavus Shaw II <gshaw1997@gmail.com>
2018-10-15 16:17:21 -0400

    :sparkles: Add LearningObjectUpdates type

---
+++ src/types/learning-object-updates.ts
@@ -0,0 +1,28 @@
+import { LearningObjectPDF } from '@cyber4all/clark-entity/dist/learning-object';
+
+export interface LearningObjectUpdates {
+  name?: string;
+  date?: string;
+  length?: string;
+  levels?: string[];
+  description?: string;
+  contributors?: string[];
+  status?: string;
+  published?: boolean;
+  materials?: { [index: string]: any };
+  'materials.pdf'?: LearningObjectPDF;
+}
+const LearningObjectUpdateProps: LearningObjectUpdates = {
+  name: '',
+  date: '',
+  length: '',
+  levels: [],
+  description: '',
+  contributors: [],
+  status: '',
+  published: false,
+  materials: {},
+};
+export const VALID_LEARNING_OBJECT_UPDATES = Object.keys(
+  LearningObjectUpdateProps,
+);
