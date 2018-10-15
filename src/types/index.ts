44a7be20ecb3b56861127224b81959681db240ce
Gustavus Shaw II <gshaw1997@gmail.com>
2018-10-15 16:17:32 -0400

    :sparkles: Export types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import { UserToken } from './user-token';
+import {
+  LearningObjectUpdates,
+  VALID_LEARNING_OBJECT_UPDATES,
+} from './learning-object-updates';
+
+export { UserToken, LearningObjectUpdates, VALID_LEARNING_OBJECT_UPDATES };
