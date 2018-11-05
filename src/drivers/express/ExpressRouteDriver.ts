3d8b4daa9097bf12f2a1ac5aa32b19563f4627c8
Gustavus Shaw II <gshaw1997@gmail.com>
2018-11-05 17:20:02 -0500

    :sparkles: Respond with 404 page when file does not exist

--- src/drivers/express/ExpressRouteDriver.ts
+++ src/drivers/express/ExpressRouteDriver.ts
@@ -249,6 +249,22 @@ export class ExpressRouteDriver {
               .send(
                 'Invalid Access. You do not have download privileges for this file',
               );
+          } else if (e.message === 'File not found') {
+            const redirectUrl = LEARNING_OBJECT_ROUTES.CLARK_DETAILS({
+              objectName: e.object.name,
+              username: req.params.username,
+            });
+            res.send(`
+            <div><h1>File Not Found</h1></div>
+            <div>
+              <p>
+                The requested file does not exist.
+                <br/>
+                <br/>
+                You can find the latest materials for this learning object <a href="${redirectUrl}">here.</a>
+              </p>
+            </div>
+            `);
           } else {
             console.error(e);
             reportError(e);
