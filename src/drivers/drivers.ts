cd91b11c0bc61baa51d846ab5773573bfca02037
Gustavus Shaw II <gshaw1997@gmail.com>
2018-11-21 23:36:23 -0500

    Revert ":sparkles: Implement IORedisDriver"
    This reverts commit 1c3edb48e6f9f75b8aa13398657724f2ca73835f.

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -4,7 +4,6 @@ import { ExpressAuthRouteDriver } from './express/ExpressAuthRouteDriver';
 import { ExpressAdminRouteDriver } from './express/ExpressAdminRouteDriver';
 import { MongoDriver } from './MongoDriver';
 import { S3Driver } from '../FileManager/S3Driver';
-import { IORedisDriver } from '../FileManager/IORedisDriver';
 export {
   ExpressDriver,
   ExpressRouteDriver,
@@ -12,5 +11,4 @@ export {
   ExpressAdminRouteDriver,
   MongoDriver,
   S3Driver,
-  IORedisDriver,
 };
