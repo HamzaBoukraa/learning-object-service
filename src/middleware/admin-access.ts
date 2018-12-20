e30197832ba3370e31660d7f0d72a1ea0098d511
Gustavus Shaw II <gshaw1997@gmail.com>
2018-12-20 16:49:47 -0500

    :memo: Document enforceAdminAccess function

--- src/middleware/admin-access.ts
+++ src/middleware/admin-access.ts
@@ -1,5 +1,17 @@
+import 'dotenv/config';
 import * as jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
+
+/**
+ * Checks if decoded token is set in request and contains 'admin' permissions.
+ * If user is not set, the token is verified from cookie presence or Bearer token
+ * If no user and no valid token or invalid permissions, responds with 401
+ *
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ */
 export function enforceAdminAccess(req: Request, res: Response, next: Function) {
   let user = req.user;
   const cookie = req.cookies.presence;
