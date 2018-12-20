24c612df3946f307afe770186adb71bc5655e257
Gustavus Shaw II <gshaw1997@gmail.com>
2018-12-20 16:50:35 -0500

    :recycle: Export middleware from index

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+import { processToken } from './process-token';
+import { enforceAuthenticatedAccess } from './authenticated-access';
+import { enforceAdminAccess } from './admin-access';
+
+export { processToken, enforceAuthenticatedAccess, enforceAdminAccess };
