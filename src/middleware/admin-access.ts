af5463263e0e158c174bbcc78f1ff3c1071e410e
Gustavus Shaw II <gshaw1997@gmail.com>
2018-12-20 16:49:26 -0500

    :bug: Change decode to verify so that token is validated

--- src/middleware/admin-access.ts
+++ src/middleware/admin-access.ts
@@ -4,7 +4,7 @@ export function enforceAdminAccess(req: Request, res: Response, next: Function) 
   let user = req.user;
   const cookie = req.cookies.presence;
   if (!user && cookie) {
-    user = jwt.decode(cookie);
+    user = jwt.verify(cookie, process.env.KEY);
   }
   if (user && user.accessGroups && user.accessGroups.includes('admin')) {
     next();
