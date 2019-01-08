78ac59fb84bc9f3f518fc5346e615ed6cbc3e29a
Sean Donnelly <sdonnelly3000@gmail.com>
2019-01-08 13:26:35 -0500

    :bug: Fix timeout error in tests
    The function being awaited was misplaced, and needed to be the submitForReview function.

--- src/LearningObjectSubmission/SubmissionInteractor.spec.ts
+++ src/LearningObjectSubmission/SubmissionInteractor.spec.ts
@@ -7,16 +7,15 @@ import { MockDataStore } from '../tests/mock-drivers/MockDataStore';
 const dataStore: DataStore = new MockDataStore; // DataStore
 
 describe('submitForReview', () => {
-  it('should submit given a valid username and id', async done => {
+  it('should submit given a valid username and id', async () => {
     try {
-      await expect(submitForReview(
+      expect(await submitForReview(
         dataStore,
         MOCK_OBJECTS.USERNAME,
         SUBMITTABLE_LEARNING_OBJECT.id,
-        SUBMITTABLE_LEARNING_OBJECT.collection
+        SUBMITTABLE_LEARNING_OBJECT.collection,
       ))
       .resolves.toBe(undefined);
-      done();
     } catch (error) {
       console.log(error);
     }
