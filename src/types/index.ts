8a99d72c70c547a5b402d05ce8f52b571c86ccc9
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-08 17:00:26 -0500

    :sparkles: Add Document types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,23 @@ import {
   LearningObjectUpdates,
   VALID_LEARNING_OBJECT_UPDATES,
 } from './learning-object-updates';
+import {
+  LearningObjectDocument,
+  MaterialDocument,
+  FileDocument,
+  LearningObjectLockDocument,
+  UrlDocument,
+} from './learning-object-document';
+import { UserDocument } from './user-document';
 
-export { UserToken, LearningObjectUpdates, VALID_LEARNING_OBJECT_UPDATES };
+export {
+  UserToken,
+  LearningObjectUpdates,
+  VALID_LEARNING_OBJECT_UPDATES,
+  UserDocument,
+  LearningObjectDocument,
+  MaterialDocument,
+  FileDocument,
+  LearningObjectLockDocument,
+  UrlDocument,
+};
