edc4ef83b3263a5b43519ea9f069195780da0449
davidnso <davidnsoesie1@gmail.com>
2019-01-10 15:43:21 -0500

    remove all .js files

--- src/drivers/SentryConnector.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var raven = require("raven");
-exports.reportError = function (error) {
-    raven.captureException(error);
-};
--- src/interactors/interactors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var LearningObjectInteractor_1 = require("./LearningObjectInteractor");
-exports.LearningObjectInteractor = LearningObjectInteractor_1.LearningObjectInteractor;
-var AdminLearningObjectInteractor_1 = require("./AdminLearningObjectInteractor");
-exports.AdminLearningObjectInteractor = AdminLearningObjectInteractor_1.AdminLearningObjectInteractor;
