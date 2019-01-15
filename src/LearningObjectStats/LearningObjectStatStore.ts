8eea494cc2a9683804eda1a46c6000e48864048c
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-15 10:31:52 -0500

    :recycle: Refactor fetchStats

--- src/LearningObjectStats/LearningObjectStatStore.ts
+++ src/LearningObjectStats/LearningObjectStatStore.ts
@@ -18,29 +18,43 @@ export class LearningObjectStatStore implements LearningObjectStatDatastore {
   async fetchStats(params: {
     query: any;
   }): Promise<Partial<LearningObjectStats>> {
-    const statCursor = await this.db
+    // Perform aggregation on Learning Objects collection to get length distribution, total number of objects, and number of released objects
+    const statCursor = this.db
       .collection(COLLECTIONS.LearningObject.name)
-      .aggregate([
+      .aggregate<{
+        _id: string;
+        ids: string[];
+        count: number;
+        released: number;
+      }>([
         { $match: params.query },
         {
           $group: {
             _id: '$length',
             count: { $sum: 1 },
+            released: {
+              $sum: {
+                $cond: [{ $eq: ['$status', 'released'] }, 1, 0],
+              },
+            },
             ids: { $push: '$_id' },
           },
         },
       ]);
-    const bloomsData = await this.db
+    // Fetch blooms distribution data
+    const bloomsCursor = await this.db
       .collection<{ _id: string; value: number }>(
         BLOOMS_DISTRIBUTION_COLLECTION,
       )
-      .find()
-      .toArray();
-    const statsArr: {
-      _id: string;
-      ids: string[];
-      count: number;
-    }[] = await statCursor.toArray();
+      .find();
+
+    // Convert cursors to arrays
+    const [objectStats, bloomsData] = await Promise.all([
+      statCursor.toArray(),
+      bloomsCursor.toArray(),
+    ]);
+
+    // Create stats object with default values
     const stats: Partial<LearningObjectStats> = {
       ids: [],
       lengths: {
@@ -55,31 +69,39 @@ export class LearningObjectStatStore implements LearningObjectStatDatastore {
         evaluate: 0,
         remember: 0,
       },
+      total: 0,
+      released: 0,
     };
-
+    // If objectStats is defined and is iterable
+    if (objectStats && objectStats.length) {
+      // For each stats grouped by length
+      objectStats.forEach(stat => {
+        // Add object ids to stat's ids array
+        stats.ids.push(...stat.ids);
+        // Set stat.lengths[nanomodule | micromodule | module | unit | course] equal to count from aggregation
+        stats.lengths[stat._id] = stat.count;
+        // Increment total by number in count
+        stats.total += stat.count;
+        // Increment released by number in released
+        stats.released += stat.released;
+      });
+    }
+    // If bloomsData is defined and is iterable
     if (bloomsData && bloomsData.length) {
-      for (let i = 0; i < bloomsData.length; i++) {
-        switch (bloomsData[i]._id) {
+      // For each bloom level match _id to level in stats object and set appropriate stat.blooms_distribution value to that of the document value
+      bloomsData.forEach(bloom => {
+        switch (bloom._id) {
           case BLOOMS.APPLY:
-            stats.blooms_distribution.apply = bloomsData[i].value;
+            stats.blooms_distribution.apply = bloom.value;
             break;
           case BLOOMS.EVALUATE:
-            stats.blooms_distribution.evaluate = bloomsData[i].value;
+            stats.blooms_distribution.evaluate = bloom.value;
             break;
           case BLOOMS.REMEMBER:
-            stats.blooms_distribution.remember = bloomsData[i].value;
+            stats.blooms_distribution.remember = bloom.value;
             break;
           default:
         }
-      }
-    } else {
-      console.log('BLOOMS_DATA is undefined. ');
-    }
-
-    if (statsArr && statsArr.length) {
-      statsArr.map(stat => {
-        stats.ids.push(...stat.ids);
-        stats.lengths[stat._id] = stat.count;
       });
     }
 
