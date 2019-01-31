6b90e9c13f4bf83651e61e6f56187f4085206b9f
Nick Visalli <nick@nicks-mbp.towson.edu>
2019-01-31 15:12:20 -0500

    refactor authGuard

--- src/interactors/AuthGuard.ts
+++ src/interactors/AuthGuard.ts
@@ -11,27 +11,27 @@ export enum accessGroupsComparison {
 }
 
 /**
-   * Compares two string arrays and checks for a matching pair
-   * If a matching pair exists, access to the requested function is granted
-   * Else, an error is thrown
-   * 
-   * Curator and Reviewer strings are structured differently (i.e. curator@abvCollectionName). Because of this,
-   * they require a special case
-   * 
-   * If canBeUser, a signed in user can perform the action. Return true so that the caller can handle the case of a user 
-   * without throwing an error
-   *
-   * @param {string[]} userAccessGroups the access groups that belong to the current user
-   * @param {string[]} requiredAccessGroups the access groups that required for the needed function
-   *
-   * @returns {void | Error} Throws an error if the current user does not have permission to access the needed function
-   */
+ * Compares two string arrays and checks for a matching pair
+ * If a matching pair exists, access to the requested function is granted
+ * Else, an error is thrown
+ * 
+ * Curator and Reviewer strings are structured differently (i.e. curator@abvCollectionName). Because of this,
+ * they require a special case
+ * 
+ * If canBeUser, a signed in user can perform the action. Return true so that the caller can handle the case of a user 
+ * without throwing an error
+ *
+ * @param {string[]} userAccessGroups the access groups that belong to the current user
+ * @param {string[]} requiredAccessGroups the access groups that required for the needed function
+ *
+ * @returns {void | Error} Throws an error if the current user does not have permission to access the needed function
+ */
 export function verifyAccessGroup(
     userAccessGroups: string[],
     requiredAccessGroups: string[],
     requestedCollection?: string,
     canBeUser?: boolean
-): void | Error | boolean {
+): boolean {
 
     let hasAccess = false;
 
@@ -72,23 +72,17 @@ export function verifyAccessGroup(
         }
     }
 
-    if(canBeUser) {
-        return true;
-    }
-
-    if (!hasAccess) {
-        throw new Error(LearningObjectError.INVALID_ACCESS(userAccessGroups, requiredAccessGroups));
-    }
+    return canBeUser || hasAccess;
 }
 
 /**
-   * Recursive implementation of merge sort algorithm. We use this function to sort accessGroup arrays
-   * by level of access. Higher access groups should come first in the array (i.e. ['editor', 'admin'] -> ['admin', 'editor'])
-   *
-   * @param {string[]} accessGroups array of access groups to be sorted
-   *
-   * @returns {string[]} sorted array of access groups
-   */
+ * Recursive implementation of merge sort algorithm. We use this function to sort accessGroup arrays
+ * by level of access. Higher access groups should come first in the array (i.e. ['editor', 'admin'] -> ['admin', 'editor'])
+ *
+ * @param {string[]} accessGroups array of access groups to be sorted
+ *
+ * @returns {string[]} sorted array of access groups
+ */
 function mergeSort(accessGroups: string[]): string[] {
     if (accessGroups.length  === 1) {
         return accessGroups;
@@ -103,14 +97,14 @@ function mergeSort(accessGroups: string[]): string[] {
 
 
 /**
-   * Responsible for merging the left and right arrays that are generated in the mergeSort function
-   * 
-   * @param {string[]} left array containing elements 0 to center from accessGroups array
-   * @param {string[]} right array containing elements center + 1 to length - 1 from accessGroups array
-   *
-   * @returns {string[]} array containing all elements from left and right arrays. NOTE: order of left 
-   * and right in return statement does not matter because one of them will always be empty at that point
-   */
+ * Responsible for merging the left and right arrays that are generated in the mergeSort function
+ * 
+ * @param {string[]} left array containing elements 0 to center from accessGroups array
+ * @param {string[]} right array containing elements center + 1 to length - 1 from accessGroups array
+ *
+ * @returns {string[]} array containing all elements from left and right arrays. NOTE: order of left 
+ * and right in return statement does not matter because one of them will always be empty at that point
+ */
 function merge(
     left: string[],
     right: string[]
@@ -132,21 +126,24 @@ function merge(
 }
 
 /**
-   * Compares the name of the collection on the userGroup string to the name of the collection
-   * that is being requested
-   * If they match, return true
-   * Otherwise, return false
-   *
-   * @param {string} userGroup the access group that belong to the current user
-   * @param {string} requestedCollection the name of the collection that is being requested by the user
-   *
-   * @returns {boolean} true is user has access to the collection, false if otherwise
-   */
+ * Compares the name of the collection on the userGroup string to the name of the collection
+ * that is being requested
+ * If they match, return true
+ * Otherwise, return false
+ *
+ * @param {string} userGroup the access group that belong to the current user
+ * @param {string} requestedCollection the name of the collection that is being requested by the user
+ *
+ * @returns {boolean} true is user has access to the collection, false if otherwise
+ */
 function verifyCollectionAccess(
     userGroup: string,
     requestedCollection: string
 ): boolean {
-    const collectionName = parseCollectionName(userGroup);
+    if (!userGroup.includes('reviewer') || !userGroup.includes('curator')) {
+        throw new Error('Invalid string');
+    }
+    const collectionName = parseUserGroup(userGroup, true);
     if (collectionName === requestedCollection) {
         return true;
     }
@@ -154,54 +151,44 @@ function verifyCollectionAccess(
 }
 
 /**
-   * Takes a string and parses the collection name out of it. The userGroup is assumed to be
-   * curator or reviewer
-   *
-   * @param {string} userGroup the access group that belong to the current user
-   *
-   * @returns {string} the collection name 
-   */
-function parseCollectionName(userGroup: string): string {
+ * Takes a string and parses the collection name out of it. The userGroup is assumed to be
+ * curator or reviewer
+ *
+ * @param {string} userGroup the access group that belong to the current user
+ *
+ * @returns {string} the collection name 
+ */
+function parseUserGroup(userGroup: string, byCollection: boolean): string {
     const list = userGroup.split('@');
-    const collectionName = list[1];
-    return collectionName;
-}   
+    if (byCollection) {
+        const collectionName = list[1];
+        return collectionName;
+    } else {
+        const roleName = list[0];
+        return roleName;
+    }   
+}  
 
 /**
-   * Checks for strings containing 'curator' or 'reviwer'. Parses the role name out 
-   * of those strings and returns them in an array format
-   *
-   * @param {string} left string representing access group
-   * @param {string} right string representing access group
-   *
-   * @returns {string[]} string array containing formatted access group strings
-   */
+ * Checks for strings containing 'curator' or 'reviwer'. Parses the role name out 
+ * of those strings and returns them in an array format
+ *
+ * @param {string} left string representing access group
+ * @param {string} right string representing access group
+ *
+ * @returns {string[]} string array containing formatted access group strings
+ */
 function formatSortString(left: string, right: string): string[] {
     let copyLeft = left;
     let copyRight = right;
 
     if (left.includes('curator') || left.includes('reviewer')) {
-        copyLeft = parseRoleName(left);
+        copyLeft = parseUserGroup(left, false);
     } 
 
     if (right.includes('curator') || right.includes('reviewer')) {
-        copyRight = parseRoleName(right);
+        copyRight = parseUserGroup(right, false);
     }
 
     return [copyLeft, copyRight];
 }
-
-/**
-   * Takes a string and parses the role name out of it. The userGroup is assumed to be
-   * curator or reviewer
-   *
-   * @param {string} userGroup the access group that belong to the current user
-   *
-   * @returns {string} the role name 
-   */
-function parseRoleName(userGroup: string): string {
-    const list = userGroup.split('@');
-    const roleName = list[0];
-    return roleName;
-}
-
