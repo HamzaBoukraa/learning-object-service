a48e50f44e84e94b05e401badc2be021fe86350a
Sean Donnelly <sdonnelly3000@gmail.com>
2019-02-18 15:59:04 -0500

    :memo: Document Error Types

--- src/errors.ts
+++ src/errors.ts
@@ -1,8 +1,15 @@
 import { reportError } from './drivers/SentryConnector';
 
+/**
+ * The reasons for which a ServiceError may be thrown.
+ */
 export enum ServiceErrorReason {
   INTERNAL = 'InternalServiceError',
 }
+
+/**
+ * Defines an error that the service encountered when processing a request.
+ */
 export class ServiceError extends Error {
   constructor(type: ServiceErrorReason) {
     super('Internal Server Error');
@@ -10,11 +17,17 @@ export class ServiceError extends Error {
   }
 }
 
+/**
+ * The reasons for which a ResourceError may be thrown.
+ */
 export enum ResourceErrorReason {
   INVALID_ACCESS = 'InvalidAccess',
   NOT_FOUND = 'NotFound',
   BAD_REQUEST = 'BadRequest',
 }
+/**
+ * Defines an error involving a specific resource in the service.
+ */
 export class ResourceError extends Error {
   constructor(message: string, type: ResourceErrorReason) {
     super(message);
@@ -22,7 +35,20 @@ export class ResourceError extends Error {
   }
 }
 
+/**
+ * Takes an error object of any kind, and maps it to a status code and message.
+ *
+ * Any error that is not of type ResourceError or ServiceError will default to
+ * an Internal Server Error. If a value is passed in that is not of type Error,
+ * it will be reported and and processed as an Internal Server Error.
+ * @param error the Error to map to HTTP information.
+ * @returns information to convert an Error to a proper HTTP response.
+ */
 export function mapErrorToStatusCode(error: Error): { code: number, message: string } {
+  if (!(error instanceof Error)) {
+    reportError(error);
+    return { code: 500, message: 'Internal Server Error' };
+  }
   const status = {
     code: 500,
     message: error.message,
@@ -42,6 +68,5 @@ export function mapErrorToStatusCode(error: Error): { code: number, message: str
       status.code = 500;
       status.message = 'Internal Service Error';
   }
-
   return status;
 }
