0022a9e3d00c0fd0d76ed593a18f31a78ab0581b
Gustavus Shaw II <gshaw1997@gmail.com>
2019-02-22 16:53:54 -0500

    Update MockS3Driver

--- src/tests/mock-drivers/MockS3Driver.ts
+++ src/tests/mock-drivers/MockS3Driver.ts
@@ -1,48 +1,58 @@
 import { FileManager } from '../../interfaces/interfaces';
-import {
-    FileUpload, CompletedPartList,
-} from '../../interfaces/FileManager';
+import { FileUpload, CompletedPartList } from '../../interfaces/FileManager';
 import { MOCK_OBJECTS } from '../mocks';
 import { CompletedPart } from 'aws-sdk/clients/s3';
-
+import { Readable } from 'stream';
 
 export class MockS3Driver implements FileManager {
-    streamFile(params: { path: string }): import('stream').Readable {
-        throw new Error('Method not implemented.');
-    }
-    hasAccess(path: string): Promise<boolean> {
-        throw new Error('Method not implemented.');
-    }
-    initMultipartUpload(params: { path: string; }): Promise<string> {
-        throw new Error('Method not implemented.');
-    }
-    abortMultipartUpload(params: { path: string; uploadId: string; }): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-    upload(params: { file: FileUpload; }): Promise<string> {
-        return Promise.resolve(MOCK_OBJECTS.S3_LOCATION);
-    }
+  streamWorkingCopyFile(params: { path: string }): Readable {
+    return new Readable();
+  }
+  copyToReleased(params: {
+    srcFolder: string;
+    destFolder: string;
+  }): Promise<void> {
+    return Promise.resolve();
+  }
+  streamFile(params: { path: string }): Readable {
+    return new Readable();
+  }
+  hasAccess(path: string): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+  initMultipartUpload(params: { path: string }): Promise<string> {
+    return Promise.resolve('');
+  }
+  abortMultipartUpload(params: {
+    path: string;
+    uploadId: string;
+  }): Promise<void> {
+    return Promise.resolve();
+  }
+  upload(params: { file: FileUpload }): Promise<string> {
+    return Promise.resolve(MOCK_OBJECTS.S3_LOCATION);
+  }
 
-    delete(params: { path: string; }): Promise<void> {
-        return Promise.resolve();
-    }
+  delete(params: { path: string }): Promise<void> {
+    return Promise.resolve();
+  }
 
-    deleteAll(params: { path: string; }): Promise<void> {
-        return Promise.resolve();
-    }
-    uploadPart(params: {
-        path: string;
-        data: any;
-        partNumber: number;
-        uploadId: string;
-      }): Promise<CompletedPart> {
-        throw new Error('Method not implemented.');
-      }
-      completeMultipartUpload(params: {
-        path: string;
-        uploadId: string;
-        completedPartList: CompletedPartList;
-      }): Promise<string> {
-        throw new Error('Method not implemented.');
-      }
+  deleteAll(params: { path: string }): Promise<void> {
+    return Promise.resolve();
+  }
+  uploadPart(params: {
+    path: string;
+    data: any;
+    partNumber: number;
+    uploadId: string;
+  }): Promise<CompletedPart> {
+    throw new Error('Method not implemented.');
+  }
+  completeMultipartUpload(params: {
+    path: string;
+    uploadId: string;
+    completedPartList: CompletedPartList;
+  }): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
 }
