3012d418a8c9a3eff9b92579553a4a507408d544
Gustavus Shaw II <gshaw1997@gmail.com>
2019-04-08 13:16:31 -0400

    Update start log

--- src/drivers/express/ExpressDriver.ts
+++ src/drivers/express/ExpressDriver.ts
@@ -91,7 +91,7 @@ export class ExpressDriver {
      * Listen on provided port, on all network interfaces.
      */
     server.listen(port, () =>
-      console.log(`Learning Object Service running on localhost:${port}`),
+      console.log(`Learning Object Service running at http://localhost:${port}`),
     );
 
     return this.app;
