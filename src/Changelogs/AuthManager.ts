9a61bc1e1392917950e3184e0307b68894214701
Nick Visalli <nvisal1@students.towson.edu>
2019-05-20 17:14:46 -0400

    Update variable names

--- src/Changelogs/AuthManager.ts
+++ src/Changelogs/AuthManager.ts
@@ -14,40 +14,19 @@ export async function hasChangelogAccess(params: {
     dataStore: DataStore,
     learningObjectId: string,
 }) {
-    return await determineRole({
-        accessGroups: params.user.accessGroups,
-        dataStore: params.dataStore,
-        user: params.user,
-        learningObjectId: params.learningObjectId,
-    });
-}
-
-/**
- * Checks if accessGroups contains admin or editor
- *
- * @param {string[]} accessGroups
- * @returns {boolean}
- */
-export async function determineRole(params: {
-    dataStore: DataStore,
-    user: UserToken,
-    learningObjectId: string,
-    accessGroups: string[],
-}): Promise<string> {
-
     const userId = await params.dataStore.findUser(params.user.username);
-    const learningObject = await params.dataStore.checkLearningObjectExistence({
+    const isOwnedByAuthor = await params.dataStore.checkLearningObjectExistence({
         userId,
         learningObjectId: params.learningObjectId,
     });
 
-    if (learningObject) {
+    if (isOwnedByAuthor) {
         return ROLE.AUTHOR;
     }
 
-    if (params.accessGroups && params.accessGroups.includes(ROLE.ADMIN)) {
+    if (params.user.accessGroups && params.user.accessGroups.includes(ROLE.ADMIN)) {
         return ROLE.ADMIN;
-    } else if (params.accessGroups && params.accessGroups.includes(ROLE.EDITOR)) {
+    } else if (params.user.accessGroups && params.user.accessGroups.includes(ROLE.EDITOR)) {
         return ROLE.EDITOR;
     }
 
