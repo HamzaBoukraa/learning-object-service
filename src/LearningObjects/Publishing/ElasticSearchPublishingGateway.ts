5ca0f9ebdc1b65fb063783f6c0d1f9ff03254753
Sean Donnelly <sdonnelly3000@gmail.com>
2019-05-30 12:40:15 -0400

    Create Publishing Module (#330)
    * :building_construction: Create Publishing Module
    
    * :ok_hand: Rename to class for specificity
    
    * 2.18.4-2

---
+++ src/LearningObjects/Publishing/ElasticSearchPublishingGateway.ts
@@ -0,0 +1,17 @@
+import { PublishingDataStore } from './interactor';
+import { LearningObject } from '../../shared/entity';
+import * as request from 'request-promise';
+
+const INDEX_LOCATION = `${process.env.ELASTICSEARCH_DOMAIN}/released-objects`;
+
+/**
+ * Sends a PUT request to ElasticSearch to index the Learning Object at a
+ * specific ID. Here we use the id already associated with the Learning Object
+ * for ease of lookup.
+ */
+export class ElasticSearchPublishingGateway implements PublishingDataStore {
+  async addToReleased(releasableObject: LearningObject): Promise<void> {
+    const URI = `${INDEX_LOCATION}/_doc/${releasableObject.id}`;
+    await request.put(URI, { body: releasableObject });
+  }
+}
