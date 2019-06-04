2df340bd1933c09a04698550adf3a1da0ceb65fc
pzalep1 <43140629+pzalep1@users.noreply.github.com>
2019-06-04 09:56:36 -0400

    Add Learning Outcome Guidelines to filter for Learning Objects (#318)
    * Merge
    
    * Merge master
    
    * Add guidelines ReleasedLearningObjectQuery
    
    * Add guidelines to query
    
    * Fix CodeFactor issues
    
    * Add function to sort guidelines
    
    * Change $all to $in
    
    * Implement filtering by guideline source

--- src/shared/interfaces/DataStore.ts
+++ src/shared/interfaces/DataStore.ts
@@ -205,6 +205,7 @@ export interface ReleasedLearningObjectQuery extends Filters {
   author?: string;
   length?: string[];
   level?: string[];
+  guidelines?: string[];
   standardOutcomeIDs?: string[];
   text?: string;
   full?: boolean;
