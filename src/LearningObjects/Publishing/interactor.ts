275640404ce79289e63afdea06d90a1de3cbbe37
Nick Visalli <nvisal1@students.towson.edu>
2019-06-05 07:33:43 -0400

    Use https std lib instead of fetch

--- src/LearningObjects/Publishing/interactor.ts
+++ src/LearningObjects/Publishing/interactor.ts
@@ -1,10 +1,11 @@
 import { DataStore } from '../../shared/interfaces/DataStore';
 import { LearningObject } from '../../shared/entity';
 import { ElasticMongoReleaseRequestDuplicator } from './ElasticMongoReleaseRequestDuplicator';
-import { ResourceError, ResourceErrorReason } from '../../shared/errors';
+import { ResourceError, ResourceErrorReason, ServiceError, ServiceErrorReason } from '../../shared/errors';
 import { isAdminOrEditor } from '../../shared/AuthorizationManager';
 import { UserToken } from '../../shared/types';
 import { generateServiceToken } from '../../drivers/TokenManager';
+import * as https from 'https';
 
 export interface PublishingDataStore {
     addToReleased(releasableObject: LearningObject): Promise<void>;
@@ -21,20 +22,54 @@ export async function releaseLearningObject({ userToken, dataStore, releasableOb
     releasableObject: LearningObject;
 }): Promise<void> {
     if (isAdminOrEditor(userToken.accessGroups)) {
-        fetch(process.env.RELEASE_EMAIL_INVOCATION, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${generateServiceToken()}`,
-            },
-            body: JSON.stringify({
-                learningObjectName: releasableObject.name,
-                authorName: releasableObject.author.name,
-                collection: releasableObject.collection,
-                authorEmail: releasableObject.author.email,
-                username: releasableObject.author.username,
-            }),
+        const postData = JSON.stringify({
+            learningObjectName: releasableObject.name,
+            authorName: releasableObject.author.name,
+            collection: releasableObject.collection,
+            authorEmail: releasableObject.author.email,
+            username: releasableObject.author.username,
         });
+
+        const options = {
+          hostname: process.env.RELEASE_EMAIL_INVOCATION.split('/')[0],
+          method: 'POST',
+          path: `/${process.env.RELEASE_EMAIL_INVOCATION.split('/')[1]}`,
+          headers: {
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Content-Length': postData.length,
+               'Authorization': `Bearer ${generateServiceToken()}`,
+            },
+        };
+
+        const req = https.request(options, (res) => {
+            switch (res.statusCode) {
+                case 400:
+                    throw new ResourceError(
+                        res.statusMessage,
+                        ResourceErrorReason.BAD_REQUEST,
+                    );
+                case 401:
+                    throw new ResourceError(
+                        res.statusMessage,
+                        ResourceErrorReason.INVALID_ACCESS,
+                    );
+                case 403:
+                    throw new ResourceError(
+                        res.statusMessage,
+                        ResourceErrorReason.FORBIDDEN,
+                    );
+                case 500:
+                    throw new ServiceError(ServiceErrorReason.INTERNAL);
+                default:
+            }
+        });
+
+        req.on('error', (e) => {
+          throw new ServiceError(ServiceErrorReason.INTERNAL);
+        });
+
+        req.write(postData);
+        req.end();
         return dataStore.addToReleased(releasableObject);
     }
     throw new ResourceError(`${userToken.username} does not have access to release this Learning Object`, ResourceErrorReason.INVALID_ACCESS);
