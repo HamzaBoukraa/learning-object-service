5315f70d00113465f22a03c4490d8feac2ca1b3e
Nick Visalli <nvisal1@students.towson.edu>
2019-06-05 11:37:43 -0400

    Add CI check in gateway factory

--- src/LearningObjects/Publishing/lambda-gateway-factory.ts
+++ src/LearningObjects/Publishing/lambda-gateway-factory.ts
@@ -8,21 +8,27 @@ export class LambdaGatewayFactory {
     static instance: ReleaseEmailGateway;
     private constructor() {}
 
-    
     static getLambdaGatewayInstance() {
         if (!this.instance) {
-            switch (process.env.NODE_ENV) {
-                case 'testing':
-                    this.instance = new LambdaGatewayStub();
-                    break;
-                case 'development':
-                    this.instance = new LambdaGatewayStub();
-                    break;
-                case 'production':
-                    this.instance = new LambdaGateway();
-                    break;
-                default:
-                    throw new ServiceError(ServiceErrorReason.INTERNAL);
+            // CI is a built-in environment variable in Circle CI
+            // that allows our service to know when it is being run
+            // within a CI environment
+            if (process.env.CI) {
+                this.instance = new LambdaGatewayStub();
+            } else {
+                switch (process.env.NODE_ENV) {
+                    case 'testing':
+                        this.instance = new LambdaGatewayStub();
+                        break;
+                    case 'development':
+                        this.instance = new LambdaGatewayStub();
+                        break;
+                    case 'production':
+                        this.instance = new LambdaGateway();
+                        break;
+                    default:
+                        throw new ServiceError(ServiceErrorReason.INTERNAL);
+                }
             }
             return this.instance;
         } else {
