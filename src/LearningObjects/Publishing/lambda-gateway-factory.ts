69d03b3538f6cd458e1e9cf006ea63179a9bbd57
Nick Visalli <nvisal1@students.towson.edu>
2019-06-05 13:22:40 -0400

    Remove CI check

--- src/LearningObjects/Publishing/lambda-gateway-factory.ts
+++ src/LearningObjects/Publishing/lambda-gateway-factory.ts
@@ -10,25 +10,18 @@ export class LambdaGatewayFactory {
 
     static getLambdaGatewayInstance() {
         if (!this.instance) {
-            // CI is a built-in environment variable in Circle CI
-            // that allows our service to know when it is being run
-            // within a CI environment
-            if (process.env.CI) {
-                this.instance = new LambdaGatewayStub();
-            } else {
-                switch (process.env.NODE_ENV) {
-                    case 'testing':
-                        this.instance = new LambdaGatewayStub();
-                        break;
-                    case 'development':
-                        this.instance = new LambdaGatewayStub();
-                        break;
-                    case 'production':
-                        this.instance = new LambdaGateway();
-                        break;
-                    default:
-                        throw new ServiceError(ServiceErrorReason.INTERNAL);
-                }
+            switch (process.env.NODE_ENV) {
+                case 'testing':
+                    this.instance = new LambdaGatewayStub();
+                    break;
+                case 'development':
+                    this.instance = new LambdaGatewayStub();
+                    break;
+                case 'production':
+                    this.instance = new LambdaGateway();
+                    break;
+                default:
+                    throw new ServiceError(ServiceErrorReason.INTERNAL);
             }
             return this.instance;
         } else {
