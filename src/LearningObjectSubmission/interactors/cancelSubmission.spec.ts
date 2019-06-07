117c424b584efe44364a8308144ebec949993acb
Sean Donnelly <sdonnelly3000@gmail.com>
2019-06-07 11:04:29 -0400

    :white_check_mark: Mock SubmissionPublisher

--- src/LearningObjectSubmission/interactors/cancelSubmission.spec.ts
+++ src/LearningObjectSubmission/interactors/cancelSubmission.spec.ts
@@ -7,20 +7,30 @@ import { MockDataStore } from '../../tests/mock-drivers/MockDataStore';
 import { DataStore } from '../../shared/interfaces/DataStore';
 import { SubmissionDataStore } from '../SubmissionDatastore';
 import { LearningObjectAdapter } from '../../LearningObjects/LearningObjectAdapter';
+import { SubmissionPublisher } from './SubmissionPublisher';
+import { LearningObject } from '../../shared/entity';
 
 const dataStore = new MockDataStore();
+const publisher: SubmissionPublisher = {
+  withdrawlSubmission: (id: string) => { return Promise.resolve(); },
+  publishSubmission: (submission: LearningObject) => null,
+};
 LearningObjectAdapter.open(dataStore, null);
 
 describe('cancelSubmission', () => {
   it('should cancel the submission given a valid username and id', async done => {
+    const spy = spyOn(publisher, 'withdrawlSubmission');
     await expect(
       cancelSubmission({
         dataStore,
+        publisher,
         userId: MOCK_OBJECTS.AUTHOR_MOCK._id,
+        user: MOCK_OBJECTS.USERTOKEN,
         emailVerified: true,
         learningObjectId: SUBMITTABLE_LEARNING_OBJECT.id,
       }),
     ).resolves.toBe(undefined);
+    expect(spy).toHaveBeenCalled();
     done();
   });
 });
