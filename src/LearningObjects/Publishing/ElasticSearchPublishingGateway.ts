2506c48d7c03abb1c828cf177e03ce64c6bfb6d3
Sean Donnelly <sdonnelly3000@gmail.com>
2019-06-10 10:58:05 -0400

    :green_heart: Default Elasticsearch URI to Host

--- src/LearningObjects/Publishing/ElasticSearchPublishingGateway.ts
+++ src/LearningObjects/Publishing/ElasticSearchPublishingGateway.ts
@@ -5,6 +5,15 @@ import { cleanLearningObject } from '../../shared/elasticsearch';
 import { Client } from '@elastic/elasticsearch';
 
 /**
+ * In the case where the ELASTICSEARCH_DOMAIN is defined in the environment,
+ * the URI will point there. Otherwise, it will default to looking for an
+ * Elasticsearch node at its default port on the current host.
+ */
+const URI = process.env.ELASTICSEARCH_DOMAIN
+  ? process.env.ELASTICSEARCH_DOMAIN
+  : 'http://localhost:9200';
+
+/**
  * Sends a PUT request to ElasticSearch to index the Learning Object at a
  * specific ID. Here we use the id already associated with the Learning Object
  * for ease of lookup.
@@ -12,7 +21,7 @@ import { Client } from '@elastic/elasticsearch';
 export class ElasticSearchPublishingGateway implements PublishingDataStore {
   client: Client;
   constructor() {
-    this.client = new Client({ node: process.env.ELASTICSEARCH_DOMAIN });
+    this.client = new Client({ node: URI });
   }
   async addToReleased(releasableObject: LearningObject): Promise<void> {
     await this.client.index({
