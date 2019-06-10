a2ab7290097dda61c1fb67529c618d245e13b43c
Gustavus Shaw II <gshaw1997@gmail.com>
2019-06-10 15:23:45 -0400

    Revert ":sparkles: Add middleware to re-route non-admins"
    This reverts commit 584b6b844e8b30e9322e5e717f0f16451f724086.

--- src/LearningObjectSearch/adapters/ExpressHttpAdapter.ts
+++ src/LearningObjectSearch/adapters/ExpressHttpAdapter.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import * as Interactor from '../Interactor';
 import { Requester } from '../typings';
 import { mapErrorToResponseData } from '../../shared/errors';
 import { LearningObjectQuery } from '../../shared/interfaces/DataStore';
-import { requesterIsAdmin } from '../../LearningObjects/AuthorizationManager';
 
 /**
  * Builds the Express Router for this module
@@ -23,16 +22,12 @@ export function buildRouter() {
  * @param {Request} req [The express request object]
  * @param {Response} res [The express response object]
  */
-async function searchObjects(req: Request, res: Response, next: NextFunction) {
+async function searchObjects(req: Request, res: Response) {
   try {
     const requester: Requester = req.user;
-    if (requesterIsAdmin(requester)) {
-      const query: LearningObjectQuery = req.query;
-      const results = await Interactor.searchObjects({ requester, query });
-      res.send(results);
-    } else {
-      next();
-    }
+    const query: LearningObjectQuery = req.query;
+    const results = await Interactor.searchObjects({ requester, query });
+    res.send(results);
   } catch (e) {
     const { code, message } = mapErrorToResponseData(e);
     res.status(code).json({ message });
