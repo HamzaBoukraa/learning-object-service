4de2fc3c291fe2ebff052fc8d74cd9f78d65434d
Nick Visalli <nvisal1@students.towson.edu>
2019-06-10 12:56:39 -0400

    Fix 500 code tests

--- src/LearningObjects/LearningObjectRouteHandler.spec.ts
+++ src/LearningObjects/LearningObjectRouteHandler.spec.ts
@@ -53,10 +53,10 @@ describe('LearningObjectRouteHandler', () => {
                     done();
                 });
         });
-        it('should return a status of 500 and an Error message', done => {
+        it('should return a status of 404 and an Error message', done => {
             request
                 .get(`/learning-objects/${testObjectID}123`)
-                .expect(500)
+                .expect(404)
                 .then(res => {
                     expect(res).toBeDefined();
                     done();
