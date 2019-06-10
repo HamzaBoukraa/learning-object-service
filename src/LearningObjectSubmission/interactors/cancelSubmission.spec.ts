0973d5db0adbb300a25417b6fda4a7522ec0c4fe
Nick Visalli <nvisal1@students.towson.edu>
2019-06-10 14:09:46 -0400

    add stubs class to cancelSubmission spec

--- src/LearningObjectSubmission/interactors/cancelSubmission.spec.ts
+++ src/LearningObjectSubmission/interactors/cancelSubmission.spec.ts
@@ -1,15 +1,11 @@
 import { cancelSubmission } from './cancelSubmission';
-import {
-  MOCK_OBJECTS,
-  SUBMITTABLE_LEARNING_OBJECT,
-} from '../../tests/mocks';
 import { MockDataStore } from '../../tests/mock-drivers/MockDataStore';
-import { DataStore } from '../../shared/interfaces/DataStore';
-import { SubmissionDataStore } from '../SubmissionDatastore';
 import { LearningObjectAdapter } from '../../LearningObjects/LearningObjectAdapter';
 import { SubmissionPublisher } from './SubmissionPublisher';
 import { LearningObject } from '../../shared/entity';
+import { Stubs } from '../../tests/stubs';
 
+const stubs = new Stubs();
 const dataStore = new MockDataStore();
 const publisher: SubmissionPublisher = {
   withdrawlSubmission: (id: string) => { return Promise.resolve(); },
@@ -24,10 +20,10 @@ describe('cancelSubmission', () => {
       cancelSubmission({
         dataStore,
         publisher,
-        userId: MOCK_OBJECTS.AUTHOR_MOCK._id,
-        user: MOCK_OBJECTS.USERTOKEN,
+        userId: stubs.learningObject.author.id,
+        user: stubs.userToken,
         emailVerified: true,
-        learningObjectId: SUBMITTABLE_LEARNING_OBJECT.id,
+        learningObjectId: stubs.learningObject.id,
       }),
     ).resolves.toBe(undefined);
     expect(spy).toHaveBeenCalled();
