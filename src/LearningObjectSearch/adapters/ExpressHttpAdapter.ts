584b6b844e8b30e9322e5e717f0f16451f724086
Gustavus Shaw II <gshaw1997@gmail.com>
2019-06-10 15:22:37 -0400

    :sparkles: Add middleware to re-route non-admins

--- src/LearningObjectSearch/adapters/ExpressHttpAdapter.ts
+++ src/LearningObjectSearch/adapters/ExpressHttpAdapter.ts
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import * as Interactor from '../Interactor';
 import { Requester } from '../typings';
 import { mapErrorToResponseData } from '../../shared/errors';
 import { LearningObjectQuery } from '../../shared/interfaces/DataStore';
+import { requesterIsAdmin } from '../../LearningObjects/AuthorizationManager';
 
 /**
  * Builds the Express Router for this module
@@ -22,12 +23,16 @@ export function buildRouter() {
  * @param {Request} req [The express request object]
  * @param {Response} res [The express response object]
  */
-async function searchObjects(req: Request, res: Response) {
+async function searchObjects(req: Request, res: Response, next: NextFunction) {
   try {
     const requester: Requester = req.user;
-    const query: LearningObjectQuery = req.query;
-    const results = await Interactor.searchObjects({ requester, query });
-    res.send(results);
+    if (requesterIsAdmin(requester)) {
+      const query: LearningObjectQuery = req.query;
+      const results = await Interactor.searchObjects({ requester, query });
+      res.send(results);
+    } else {
+      next();
+    }
   } catch (e) {
     const { code, message } = mapErrorToResponseData(e);
     res.status(code).json({ message });
