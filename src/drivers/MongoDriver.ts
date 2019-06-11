59de3dd38582f0dd9f675ff0634d4236d87229c7
Nick Visalli <nvisal1@students.towson.edu>
2019-06-11 16:45:00 -0400

    Extend fetch collection meta search

--- src/drivers/MongoDriver.ts
+++ src/drivers/MongoDriver.ts
@@ -2134,7 +2134,7 @@ export class MongoDriver implements DataStore {
     try {
       const meta: any = await this.db
         .collection(COLLECTIONS.LO_COLLECTIONS)
-        .findOne({ name }, <any>{ name: 1, abstracts: 1 });
+        .findOne({ $or: [ { name }, {abvName: name} ] }, <any>{ name: 1, abstracts: 1 });
       return meta;
     } catch (e) {
       return Promise.reject(e);
