bdf780499d09b8dc43b3df0a8bd14c736d6acfa5
Gustavus Shaw II <gshaw1997@gmail.com>
2019-06-13 10:54:02 -0400

    Revert :fire: Remove ExpressDriver 1daf193

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -1,9 +1,11 @@
+import { ExpressDriver } from './express/ExpressDriver';
 import { ExpressRouteDriver } from './express/ExpressRouteDriver';
 import { ExpressAuthRouteDriver } from './express/ExpressAuthRouteDriver';
 import { ExpressAdminRouteDriver } from './express/ExpressAdminRouteDriver';
 import { MongoDriver } from './MongoDriver';
 import { S3Driver } from '../FileManager/S3Driver';
 export {
+  ExpressDriver,
   ExpressRouteDriver,
   ExpressAuthRouteDriver,
   ExpressAdminRouteDriver,
