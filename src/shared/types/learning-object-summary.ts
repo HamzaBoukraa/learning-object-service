3430852325675faf5a8ed9ec6e52325f8c42a4b1
Gustavus Shaw II <gshaw1997@gmail.com>
2019-06-18 19:43:50 -0400

    :ok_hand: :memo: Document interface

--- src/shared/types/learning-object-summary.ts
+++ src/shared/types/learning-object-summary.ts
@@ -1,19 +1,74 @@
+/**
+ * An interface representation of the AuthorSummary type
+ *
+ * @export
+ * @interface AuthorSummary
+ */
 export interface AuthorSummary {
+  /**
+   * Unique identifier of the author/user
+   */
   id: string;
+  /**
+   * Human readable unique identifier of the author/user
+   */
   username: string;
+  /**
+   * The full name of the author/user
+   */
   name: string;
+  /**
+   * The organization of the author/user
+   */
   organization: string;
 }
 
+/**
+ * An interface representation of the LearningObjectSummary type
+ *
+ * @export
+ * @interface LearningObjectSummary
+ */
 export interface LearningObjectSummary {
+  /**
+   * Unique identifier of the Learning Object
+   */
   id: string;
+  /**
+   * Summary information of the user who authored the Learning Object
+   */
   author: AuthorSummary;
+  /**
+   * The collection the Learning Object was submitted to
+   */
   collection: string;
+  /**
+   * Summary information about users who have made contributions to the Learning Object
+   */
   contributors: AuthorSummary[];
+  /**
+   * Time at which the Learning Object was last updated
+   * Value is a string representing the milliseconds elapsed since the UNIX epoch
+   */
   date: string;
+  /**
+   * User supplied information about the contents/purpose of the Learning Object
+   */
   description: string;
+  /**
+   * How long the content of the Learning Object
+   */
   length: string;
+  /**
+   * The name of the Learning Object
+   */
   name: string;
+  /**
+   * The version number of the Learning Object
+   */
   revision: number;
+  /**
+   * The stage within the review pipeline that the Learning Object exists
+   */
   status: string;
 }
