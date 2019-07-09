c4d55f076bca501b99a6a1bf3c3519435accc334
Gustavus Shaw II <gshaw1997@gmail.com>
2019-07-09 08:23:16 -0400

    :memo: Update docs for getLatestCCLicense

--- src/LearningObjects/Publishing/Bundler/CCLicenseRetriever.ts
+++ src/LearningObjects/Publishing/Bundler/CCLicenseRetriever.ts
@@ -2,7 +2,7 @@ import { Readable } from 'stream';
 
 export abstract class CCLicenseRetriever {
   /**
-   * Returns a readable stream of the latest creative commons license
+   * Returns a readable stream of the latest Creative Commons license
    *
    * @abstract
    * @returns {Promise<Readable>}
