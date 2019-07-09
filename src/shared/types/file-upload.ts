fba8974d04dc416e7121d9fc3675aa277d838e02
Nick Visalli <nvisal1@students.towson.edu>
2019-07-09 15:35:53 -0400

    Remove resolved fixme

--- src/shared/types/file-upload.ts
+++ src/shared/types/file-upload.ts
@@ -2,6 +2,5 @@ import { Readable } from 'stream';
 
 export interface FileUpload {
     path: string;
-    // FIXME: This should define the specific types it can take
     data: Buffer|Uint8Array|Blob|string|Readable;
 }
