6e78164d10b23d85b0cd3189718572fb92696f41
Gustavus Shaw II <gshaw1997@gmail.com>
2019-07-09 07:56:46 -0400

    :sparkles: Provide module file gateway

--- src/LearningObjects/Publishing/Bundler/BundlerModule.ts
+++ src/LearningObjects/Publishing/Bundler/BundlerModule.ts
@@ -4,12 +4,13 @@ import { Bundler } from './Bundler';
 import { CCLicenseRetriever } from './CCLicenseRetriever';
 import { HttpCCLicenseRetriever } from './HttpCCLicenseRetriever';
 import { FileGateway } from './FileGateway';
+import { ModuleFileGateway } from './ModuleFileGateway';
 
 @serviceModule({
   providers: [
     { provide: Bundler, useClass: ArchiverBundler },
     { provide: CCLicenseRetriever, useClass: HttpCCLicenseRetriever },
-    { provide: FileGateway, useClass: null },
+    { provide: FileGateway, useClass: ModuleFileGateway },
   ],
 })
 export class BundlerModule extends ServiceModule {}
