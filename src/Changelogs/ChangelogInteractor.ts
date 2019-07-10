9fad531c3c7793238c24184e872ce5e60eac0af9
Nick Visalli <nvisal1@students.towson.edu>
2019-07-10 09:14:16 -0400

    Allow users to have read access to released change logs

--- src/Changelogs/ChangelogInteractor.ts
+++ src/Changelogs/ChangelogInteractor.ts
@@ -4,6 +4,7 @@ import { ResourceError, ResourceErrorReason } from '../shared/errors';
 import { ChangeLogDocument } from '../shared/types/changelog';
 import { hasChangelogAccess } from './AuthManager';
 import * as md5 from 'md5';
+import { LearningObject } from '../shared/entity';
 
 /**
  * Instruct the data store to create a new log in the change logs collection
@@ -22,7 +23,7 @@ export async function createChangelog(params: {
   userId: string,
   changelogText: string,
 }): Promise<void> {
-  const role = await authorizeRequest({
+  const role = await authorizeWriteRequest({
     dataStore: params.dataStore,
     learningObjectId: params.learningObjectId,
     userId: params.userId,
@@ -59,7 +60,7 @@ export async function getRecentChangelog(params: {
   userId: string,
   user: UserToken,
 }): Promise<ChangeLogDocument> {
-  await authorizeRequest({
+  await authorizeReadRequest({
     dataStore: params.dataStore,
     learningObjectId: params.learningObjectId,
     userId: params.userId,
@@ -89,7 +90,7 @@ export async function getChangelogs(params: {
   user: UserToken,
   date?: string,
 }): Promise<ChangeLogDocument[]> {
-  await authorizeRequest({
+  await authorizeReadRequest({
     dataStore: params.dataStore,
     learningObjectId: params.learningObjectId,
     userId: params.userId,
@@ -103,9 +104,11 @@ export async function getChangelogs(params: {
 }
 
 /**
- * Determines if the request is valid
- * Ensure that the user making the reuqest has access to modify changelogs
- * Checks if the requested learning object exists
+ * - Determines if the request to read change logs is valid
+ * This request validation is performed by checking that the given
+ * - Ensure that the user making the reuqest has access to modify changelogs.
+ * Only the Learning Object author, editors, and admins have write access to 
+ * change logs.
  *
  * @param {DataStore} dataStore An instance of DataStore
  * @param {string} learningObjectId The id of the learning object that the requested changelog belongs to
@@ -114,31 +117,75 @@ export async function getChangelogs(params: {
  *
  * @returns {ChangeLogDocument[]}
  */
-async function authorizeRequest(params: {
+async function authorizeWriteRequest(params: {
   dataStore: DataStore,
   learningObjectId: string,
   userId: string,
   user: UserToken,
 }): Promise<string> {
-  const role = await hasChangelogAccess({
-    user: params.user,
-    dataStore: params.dataStore,
-    learningObjectId: params.learningObjectId,
-  });
-
-  const isOwnedByAuthor = await params.dataStore.checkLearningObjectExistence({
+  const learningObject = await params.dataStore.checkLearningObjectExistence({
     learningObjectId: params.learningObjectId,
     userId: params.userId,
   });
 
-  if (!isOwnedByAuthor) {
+  if (!learningObject) {
     throw new ResourceError(
       `Learning Object ${params.learningObjectId} not found for user ${params.userId}`,
       ResourceErrorReason.NOT_FOUND,
     );
   }
 
+  const role = await hasChangelogAccess({
+    user: params.user,
+    dataStore: params.dataStore,
+    learningObjectId: params.learningObjectId,
+  });
+
+
   return role;
+}
+
+/**
+ * - Determines if the request to read change logs is valid
+ * This request validation is performed by checking that the given
+ * authorId and learningObjectId pair exist.
+ * - Ensure that the user making the request has access to modify changelogs
+ * If the Learning Object is released, all users have access to read change logs.
+ * If the Learning Object is not released, only the Learning Object author, editors,
+ * and admins have access to read change logs.
+ *
+ * @param {DataStore} dataStore An instance of DataStore
+ * @param {string} learningObjectId The id of the learning object that the requested changelog belongs to
+ * @param {string} userId The id of learning object author
+ * @param {UserToken} user information about the requester
+ *
+ * @returns {ChangeLogDocument[]}
+ */
+async function authorizeReadRequest(params: {
+  dataStore: DataStore,
+  learningObjectId: string,
+  userId: string,
+  user: UserToken,
+}): Promise<void> {
+  const learningObject = await params.dataStore.checkLearningObjectExistence({
+    learningObjectId: params.learningObjectId,
+    userId: params.userId,
+  });
+
+  if (!learningObject) {
+    throw new ResourceError(
+      `Learning Object ${params.learningObjectId} not found for user ${params.userId}`,
+      ResourceErrorReason.NOT_FOUND,
+    );
+  }
+
+  if (learningObject.status !== LearningObject.Status.RELEASED) {
+    await hasChangelogAccess({
+      user: params.user,
+      dataStore: params.dataStore,
+      learningObjectId: params.learningObjectId,
+    });
+  }
 }
 
 /**
