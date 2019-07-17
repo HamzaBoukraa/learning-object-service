6faca88cf01d82800d47f6b3641479c6e799530e
Gustavus Shaw II <gshaw1997@gmail.com>
2019-07-17 11:40:12 -0400

    Convert forEach to for of to allow error to propagate (#417)
    * :bug: Convert forEach to for of to allow error to propagate
    
    * 3.1.1

--- src/FileMetadata/Interactor.ts
+++ src/FileMetadata/Interactor.ts
@@ -294,7 +294,7 @@ function generateFileMetadataInserts(
   learningObject: LearningObjectSummary,
 ) {
   const inserts: FileMetadataInsert[] = [];
-  files.forEach(async file => {
+  for (const file of files) {
     const cleanFile = sanitizeObject({ object: file }, false);
     validateFileMeta(cleanFile);
     const newInsert: FileMetadataInsert = generateFileMetaInsert(
@@ -302,7 +302,7 @@ function generateFileMetadataInserts(
       learningObject,
     );
     inserts.push(newInsert);
-  });
+  }
   return inserts;
 }
 
