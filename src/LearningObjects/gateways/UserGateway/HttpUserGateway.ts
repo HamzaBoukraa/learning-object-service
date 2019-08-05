9b88740022627a9f9c6206cb8ad43a04aa862891
Paige Zaleppa <pz0342@gmail.com>
2019-08-05 08:58:42 -0400

    Merge master

--- src/LearningObjects/gateways/UserGateway/HttpUserGateway.ts
+++ src/LearningObjects/gateways/UserGateway/HttpUserGateway.ts
@@ -2,13 +2,17 @@ import * as request from 'request-promise';
 import { User } from '../../../shared/entity';
 import { USER_ROUTES } from './Routes';
 import { UserGateway } from '../../interfaces/UserGateway';
+import { generateServiceToken } from '../../../drivers/TokenManager';
 
-export class ModuleUserGateway implements UserGateway {
-    private options = {
-        uri: '',
-        json: true,
-        method: 'GET',
-    };
+export class HttpUserGateway implements UserGateway {
+  private options = {
+    uri: '',
+    json: true,
+    headers: {
+      Authorization: 'Bearer',
+    },
+    method: 'GET',
+  };
 
   /**
    * @inheritdoc
@@ -17,6 +21,7 @@ export class ModuleUserGateway implements UserGateway {
   async getUser(username: string): Promise<User> {
     const options = { ...this.options };
     options.uri = USER_ROUTES.GET_USER(username);
+    options.headers.Authorization = `Bearer ${generateServiceToken()}`;
     const res = await request(options);
     return new User(res);
   }
--- src/LearningObjects/interfaces/index.ts
+++ src/LearningObjects/interfaces/index.ts
@@ -1,5 +1,6 @@
 import { FileManagerGateway } from './FileManagerGateway';
 import { FileMetadataGateway } from './FileMetadataGateway';
 import { ReadMeBuilder } from './ReadMeBuilder';
+import { UserGateway } from './UserGateway';
 
-export {FileManagerGateway, FileMetadataGateway, ReadMeBuilder};
+export {FileManagerGateway, FileMetadataGateway, ReadMeBuilder, UserGateway};
