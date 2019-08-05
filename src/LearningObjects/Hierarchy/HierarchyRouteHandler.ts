32914ac65b3eb102b01ca9646f8fb8578c76ba9b
Sean Donnelly <sdonnelly3000@gmail.com>
2019-08-05 11:31:03 -0400

    :bug: Fix TSLint Errors

--- src/LearningObjects/Hierarchy/HierarchyRouteHandler.ts
+++ src/LearningObjects/Hierarchy/HierarchyRouteHandler.ts
@@ -3,17 +3,19 @@ import { DataStore } from '../../shared/interfaces/DataStore';
 import { LearningObjectInteractor } from '../../interactors/interactors';
 import { fetchParents } from './HierarchyInteractor';
 import { mapErrorToResponseData } from '../../shared/errors';
+import { LearningObject } from '../../shared/entity';
 
 export function initializePublic({ router, dataStore}: { router: Router, dataStore: DataStore }) {
   const getParents = async (req: Request, res: Response) => {
     try {
       const userToken = req.user;
 
+      // FIXME: this is returning LearningObjects even though it should return summaries
       const parents = await fetchParents({
         learningObjectID: req.params.id,
         userToken,
         dataStore,
-      });
+      }) as LearningObject[];
       res.status(200).send(parents.map(obj => obj.toPlainObject()));
     } catch (e) {
       const { code, message } = mapErrorToResponseData(e);
