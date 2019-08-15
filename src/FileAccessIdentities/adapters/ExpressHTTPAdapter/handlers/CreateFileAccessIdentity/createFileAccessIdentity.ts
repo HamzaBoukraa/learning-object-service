51bda0eb76679e21881c6aab7af533749a6a2f78
davidnso <davidnsoesie1@gmail.com>
2019-08-15 14:27:48 -0400

    fix file imports

--- src/FileAccessIdentities/adapters/ExpressHTTPAdapter/handlers/CreateFileAccessIdentity/createFileAccessIdentity.ts
+++ src/FileAccessIdentities/adapters/ExpressHTTPAdapter/handlers/CreateFileAccessIdentity/createFileAccessIdentity.ts
@@ -1,41 +1,35 @@
-import {
-    Request,
-    Response,
-} from 'express';
+import { Request, Response } from 'express';
 import { handleError } from '../shared/handleError/handleError';
 import { createFileAccessIdentity } from '../../../../interactors/CreateFileAccessIdentity/createFileAccessIdentity';
 import { checkIfRequesterHasPermission } from '../shared';
 
-export function createFileAccessIdentityHandler(
-    req: Request,
-    res: Response,
-) {
-    try {
-        handleRequest(req, res);
-    } catch (error) {
-        handleError({ error, res });
-    }
+export function createFileAccessIdentityHandler(req: Request, res: Response) {
+  try {
+    handleRequest(req, res);
+  } catch (error) {
+    handleError({ error, res });
+  }
 }
 
 async function handleRequest(req: Request, res: Response) {
-    const params = parseRequestParams(req);
-    checkIfRequesterHasPermission(params.requester);
-    const resourceURL = await createFileAccessIdentity(params);
-    respondWithResourceURL({res, resourceURL});
+  const params = parseRequestParams(req);
+  checkIfRequesterHasPermission(params.requester);
+  const resourceURL = await createFileAccessIdentity(params);
+  respondWithResourceURL({ res, resourceURL });
 }
 
 function parseRequestParams(req: Request) {
-    const params = {
-        username: req.params.username,
-        fileAccessIdentity: req.body.fileAccessID,
-        requester: req.user,
-    };
-    return params;
+  const params = {
+    username: req.params.username,
+    fileAccessIdentity: req.body.fileAccessID,
+    requester: req.user,
+  };
+  return params;
 }
 
 function respondWithResourceURL(params: {
-    res: Response,
-    resourceURL: string,
+  res: Response;
+  resourceURL: string;
 }) {
-    params.res.status(200).json({ resourceURL: params.resourceURL });
+  params.res.status(200).json({ resourceURL: params.resourceURL });
 }
