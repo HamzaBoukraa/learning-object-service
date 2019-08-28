60af1b7fb9181184378a65379b3e29be0d20b553
Sean Donnelly <sdonnelly3000@gmail.com>
2019-08-28 16:35:22 -0400

    :construction: Add Authorization to downloadBundle

--- src/FileManager/interactors/downloadBundle.spec.ts
+++ src/FileManager/interactors/downloadBundle.spec.ts
@@ -4,60 +4,132 @@ import { LearningObjectGateway } from '../interfaces';
 import { LearningObjectFilter } from '../../LearningObjects/typings';
 import { LearningObject } from '../../shared/entity';
 import { downloadBundle } from './downloadBundle';
+import { ResourceErrorReason, ResourceError } from '../../shared/errors';
+import { Gateways } from './shared/dependencies';
 
 const requesterStub: UserToken = {
-    username: 'test-username',
-    name: 'test-name',
-    email: 'test-email',
-    organization: 'test-organization',
-    emailVerified: true,
-    accessGroups: [],
+  username: 'test-username',
+  name: 'test-name',
+  email: 'test-email',
+  organization: 'test-organization',
+  emailVerified: true,
+  accessGroups: [],
 };
 
 const downloadBundleParamStub = {
-    requester: requesterStub,
-    learningObjectAuthorUsername: 'unittest',
-    learningObjectId: 'unittest',
-    revision: true,
+  requester: requesterStub,
+  learningObjectAuthorUsername: 'unittest',
+  learningObjectId: 'unittest',
+  revision: true,
 };
-class LearningObjectGatewayStub implements LearningObjectGateway {
-    getWorkingLearningObjectSummary(params: { requester: UserToken; id: string; }): Promise<LearningObjectSummary> {
-        throw new Error('Method not implemented.');
-    }
-    getReleasedLearningObjectSummary(id: string): Promise<LearningObjectSummary> {
-        throw new Error('Method not implemented.');
-    }
-    getActiveLearningObjectSummary(params: { requester: UserToken; id: string; }): Promise<LearningObjectSummary> {
-        throw new Error('Method not implemented.');
-    }
-    getLearningObjectById(params: { learningObjectId: string; requester?: UserToken; filter?: LearningObjectFilter; }): Promise<LearningObject> {
-        return Promise.resolve({} as LearningObject);
-    }
-    getLearningObjectByName(params: { username: string; learningObjectName: string; requester: UserToken; revision: boolean; }): Promise<LearningObject> {
-        throw new Error('Method not implemented.');
-    }
-}
-describe('When downloadBundle is called', () => {
-    describe('and the requester provided the ID of an existing Learning Object', () => {
-        it('should not invoke getLearningObjectByName', () => {
-          FileManagerModule.providers = [
-            { provide: LearningObjectGateway, useClass: LearningObjectGatewayStub },
-          ];
-          FileManagerModule.initialize();
-          const learningObjectGateway = FileManagerModule.resolveDependency(LearningObjectGateway);
-          const spy = jest.spyOn(learningObjectGateway, 'getLearningObjectByName');
-          downloadBundle(downloadBundleParamStub);
-          expect(spy).not.toHaveBeenCalled();
-        });
-    });
-    /* describe('and the requester provided the name of the Learning Object', () => {
-        it('should invoke getLearningObjectByName', () => {
 
-        });
+const LearningObjectStub = {
+  author: {
+    username: '',
+  },
+} as LearningObject;
+class LearningObjectGatewayStub implements LearningObjectGateway {
+  getWorkingLearningObjectSummary(params: {
+    requester: UserToken;
+    id: string;
+  }): Promise<LearningObjectSummary> {
+    throw new Error('Method not implemented.');
+  }
+  getReleasedLearningObjectSummary(id: string): Promise<LearningObjectSummary> {
+    throw new Error('Method not implemented.');
+  }
+  getActiveLearningObjectSummary(params: {
+    requester: UserToken;
+    id: string;
+  }): Promise<LearningObjectSummary> {
+    throw new Error('Method not implemented.');
+  }
+  getLearningObjectById(params: {
+    learningObjectId: string;
+    requester?: UserToken;
+    filter?: LearningObjectFilter;
+  }): Promise<LearningObject> {
+    throw new Error('Method not implemented');
+  }
+  getLearningObjectByName(params: {
+    username: string;
+    learningObjectName: string;
+    requester: UserToken;
+    revision: boolean;
+  }): Promise<LearningObject> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+function initializeServiceModuleFixtures() {
+  FileManagerModule.providers = [
+    { provide: LearningObjectGateway, useClass: LearningObjectGatewayStub },
+  ];
+  FileManagerModule.initialize();
+}
+describe('When downloadBundle is called for a Working Copy', () => {
+  beforeEach(() => {
+    initializeServiceModuleFixtures();
+  });
+  afterEach(() => {
+    FileManagerModule.destroy();
+  });
+  describe('and the requester has download privilege', () => {
+    describe('and the requester provided the ID of an existing Learning Object', () => {
+      it('should not invoke getLearningObjectByName', async () => {
+        const learningObjectGateway = Gateways.learningObjectGateway();
+        const collection = 'test';
+        learningObjectGateway.getLearningObjectById = () =>
+          Promise.resolve({ ...LearningObjectStub, collection } as LearningObject);
+        const spy = jest.spyOn(
+          learningObjectGateway,
+          'getLearningObjectByName',
+        );
+        const adminRequestParams = { ...downloadBundleParamStub };
+        adminRequestParams.requester.accessGroups = ['admin'];
+        const adminPromise = downloadBundle(adminRequestParams);
+        await expect(adminPromise).resolves.toBeUndefined();
+
+        const editorRequestParams = { ...downloadBundleParamStub };
+        editorRequestParams.requester.accessGroups = ['editor'];
+        const editorPromise = downloadBundle(editorRequestParams);
+        await expect(editorPromise).resolves.toBeUndefined();
+
+        const curatorRequestParams = { ...downloadBundleParamStub };
+        curatorRequestParams.requester.accessGroups = [`curator@${collection}`];
+        const curatorPromise = downloadBundle(curatorRequestParams);
+        await expect(curatorPromise).resolves.toBeUndefined();
+
+        expect(spy).not.toHaveBeenCalled();
+      });
+    });
+    describe('and the requester provided the name of the Learning Object', () => {
+      it('should invoke getLearningObjectByName', async () => {
+        const learningObjectGateway = Gateways.learningObjectGateway();
+        const error = new ResourceError('', ResourceErrorReason.NOT_FOUND);
+        learningObjectGateway.getLearningObjectById = () =>
+          Promise.reject(error);
+        learningObjectGateway.getLearningObjectByName = () =>
+          Promise.resolve(LearningObjectStub);
+        const spy = jest.spyOn(
+          learningObjectGateway,
+          'getLearningObjectByName',
+        );
+
+        const promise = downloadBundle(downloadBundleParamStub);
+        await expect(promise).resolves.toBeUndefined();
+        expect(spy).toHaveBeenCalled();
+      });
     });
     describe('and the getLearningObjectById throws an error that is not NOT_FOUND', () => {
-        it('should throw the error', () => {
+      it('should throw the error', async () => {
+        await expect(
+          downloadBundle(downloadBundleParamStub),
+        ).rejects.toThrowError();
+      });
+    });
+  });
+  describe('and the requestor does not have download privilege', () => {
 
-        });
-    }); */
+  });
 });
--- src/FileManager/interactors/downloadBundle.ts
+++ src/FileManager/interactors/downloadBundle.ts
@@ -1,6 +1,9 @@
-import { UserToken } from '../../shared/types';
+import { UserToken, AccessGroup } from '../../shared/types';
 import { Gateways } from './shared/dependencies';
-import { ResourceErrorReason } from '../../shared/errors';
+import { ResourceErrorReason, ResourceError } from '../../shared/errors';
+import { hasLearningObjectWriteAccess, requesterIsAdminOrEditor, requesterIsAuthor } from '../../shared/AuthorizationManager';
+import { LearningObject } from '../../shared/entity';
+import { bool } from 'aws-sdk/clients/signer';
 
 type DownloadBundleParams = {
   requester: UserToken,
@@ -11,24 +14,44 @@ type DownloadBundleParams = {
 /**
  *
  */
-export function downloadBundle(params: DownloadBundleParams) {
+export async function downloadBundle(params: DownloadBundleParams) {
   const { revision } = params;
   // is it a revision or not
   if (revision) {
-    downloadWorkingCopy(params);
+    await downloadWorkingCopy(params);
   } else {
-    downloadReleasedCopy(params);
+    await downloadReleasedCopy(params);
   }
 }
 
-function downloadWorkingCopy(params: DownloadBundleParams) {
+async function downloadWorkingCopy(params: DownloadBundleParams) {
   const { requester, learningObjectAuthorUsername, learningObjectId } = params;
-  const learningObject = getLearningObject(params, true);
+  const learningObject = await getLearningObject(params, true);
+  const hasAccess = authorizeDownloadRequest(requester, learningObject);
+  if (!hasAccess) {
+    throw new ResourceError(``, ResourceErrorReason.FORBIDDEN);
+  }
 }
 
-function downloadReleasedCopy(params: DownloadBundleParams) {
+function authorizeDownloadRequest(requester: UserToken, learningObject: LearningObject): boolean {
+  return requesterIsAdminOrEditor(requester)
+    || hasCollectionAccess(requester, learningObject)
+    || requesterIsAuthor({ authorUsername: learningObject.author.username, requester });
+}
+function hasCollectionAccess(requester: UserToken, learningObject: LearningObject): boolean {
+  return (
+    requester.accessGroups.indexOf(
+      `${AccessGroup.REVIEWER}@${learningObject.collection}`,
+    ) > -1 ||
+    requester.accessGroups.indexOf(
+      `${AccessGroup.CURATOR}@${learningObject.collection}`,
+    ) > -1
+  );
+}
+
+async function downloadReleasedCopy(params: DownloadBundleParams) {
   const { requester, learningObjectAuthorUsername, learningObjectId } = params;
-  const learningObject = getLearningObject(params);
+  const learningObject = await getLearningObject(params);
 }
 
 async function getLearningObject(params: DownloadBundleParams, workingCopy = false) {
