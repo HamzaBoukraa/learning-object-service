a2cffa6270ee71d10aca7ca64fb5db007bd2f605
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-09-05 15:29:53 -0400

    :white_check_mark: fix tests in learningobject.interactor spec file

--- src/interactors/LearningObject.interactor.spec.ts
+++ src/interactors/LearningObject.interactor.spec.ts
@@ -1,43 +1,59 @@
-import { LearningObjectInteractor } from '../interactors/interactors';
 import { DataStore } from '../shared/interfaces/DataStore';
 import { MockDataStore } from '../tests/mock-drivers/MockDataStore';
 import { Stubs } from '../tests/stubs';
-
 const dataStore: DataStore = new MockDataStore();
 const stubs = new Stubs();
 
-describe('fetchObjectsByIDs', () => {
-  it('should load full learning object', done => {
-    return LearningObjectInteractor.fetchObjectsByIDs({
-      dataStore,
-      ids: [stubs.learningObject.id],
-    }).then(val => {
-      expect(val).toBeInstanceOf(Array);
-      done();
-    });
+jest.doMock('../shared/gateways/user-service/UserServiceGateway', () => ({
+  __esModule: true,
+  UserServiceGateway: {
+    getInstance: () => ({
+      findUser: jest
+      .fn()
+      .mockResolvedValue({...stubs.user}),
+    }),
+  },
+}));
+
+let interactor: any;
+
+describe('LearningObjectInteractor', () => {
+  beforeAll(async () => {
+    jest.doMock('../shared/gateways/user-service/UserServiceGateway', () => ({
+      __esModule: true,
+      UserServiceGateway: {
+        getInstance: () => ({
+          findUser: jest.fn().mockResolvedValue(stubs.user),
+        }),
+      },
+    }));
+    interactor = (await import('./LearningObjectInteractor')).LearningObjectInteractor;
   });
 
-  it('should return learning object - given empty array!', done => {
-    return LearningObjectInteractor.fetchObjectsByIDs({
-      dataStore,
-      ids: [],
-    }).then(val => {
-      expect(val).toBeInstanceOf(Array);
-      done();
+  describe('fetchObjectsByIDs', () => {
+    it('should load full learning object', async () => {
+      return expect(interactor.fetchObjectsByIDs({
+        dataStore,
+        ids: [stubs.learningObject.id],
+      })).resolves.toBeInstanceOf(Array);
     });
-  });
-});
 
-describe('getLearningObjectId', () => {
-  it('should find a learning object ID', done => {
-    return LearningObjectInteractor.getLearningObjectId({
-      dataStore,
-      username: stubs.learningObject.author.username,
-      learningObjectName: stubs.learningObject.name,
-      userToken: stubs.userToken,
-    }).then(val => {
-      expect(val).toEqual(stubs.learningObject.id);
-      done();
+    it('should return learning object - given empty array!', async () => {
+      return expect(interactor.fetchObjectsByIDs({
+        dataStore,
+        ids: [],
+      })).resolves.toBeInstanceOf(Array);
+      });
+    });
+
+  describe('getLearningObjectId', () => {
+    it('should find a learning object ID', async () => {
+      return expect(interactor.getLearningObjectId({
+        dataStore,
+        username: stubs.learningObject.author.username,
+        learningObjectName: stubs.learningObject.name,
+        userToken: stubs.userToken,
+      })).resolves.toBe(stubs.learningObject.id);
     });
   });
 });
