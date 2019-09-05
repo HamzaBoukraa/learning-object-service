dabe332f4a4c7a6ed8e639c53d071124abe99cec
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-09-05 15:30:49 -0400

    add getInstance in MockUserServiceGateway

--- src/shared/gateways/user-service/__mocks__/UserServiceGateway.ts
+++ src/shared/gateways/user-service/__mocks__/UserServiceGateway.ts
@@ -4,6 +4,12 @@ import { Stubs } from '../../../../tests/stubs';
 export class MockUserServiceGateway {
     stubs = new Stubs();
 
+    private constructor() { }
+
+    static getInstance(): MockUserServiceGateway {
+        return new MockUserServiceGateway();
+    }
+
     getUser = async (username: string): Promise<User> => {
         return Promise.resolve(this.stubs.user);
     }
