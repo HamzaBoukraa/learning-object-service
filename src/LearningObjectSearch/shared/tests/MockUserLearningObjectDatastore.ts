98ab02df93250a3668466ca1bac0d2617f74582f
davidnso <davidnsoesie1@gmail.com>
2019-09-21 10:39:11 -0400

    fix failing tests

--- src/LearningObjectSearch/shared/tests/MockUserLearningObjectDatastore.ts
+++ src/LearningObjectSearch/shared/tests/MockUserLearningObjectDatastore.ts
@@ -1,19 +1,28 @@
 import { UserLearningObjectDatastore } from '../../interfaces/UserLearningObjectDatastore';
 import { LearningObjectQuery } from '../../../shared/interfaces/DataStore';
-import { LearningObjectSummary, UserLearningObjectSearchQuery, CollectionAccessMap } from '../../../shared/types';
+import {
+  LearningObjectSummary,
+  UserLearningObjectSearchQuery,
+  CollectionAccessMap,
+} from '../../../shared/types';
 import { Stubs } from '../../../tests/stubs';
 import { mapLearningObjectToSummary } from '../../../shared/functions';
+import { LearningObject } from '../../typings';
 
-export class MockUserLearningObjectDatastore implements UserLearningObjectDatastore {
-    stubs = new Stubs();
-    searchReleasedUserObjects(query: LearningObjectQuery, username: string): Promise<LearningObjectSummary[]> {
-        return Promise.resolve(
-            [this.stubs.learningObject].map(mapLearningObjectToSummary),
-        );
-    }
-    searchAllUserObjects(query: UserLearningObjectSearchQuery, username: string, collectionRestrictions?: CollectionAccessMap): Promise<LearningObjectSummary[]> {
-        return Promise.resolve(
-            [this.stubs.learningObject].map(mapLearningObjectToSummary),
-        );
-    }
+export class MockUserLearningObjectDatastore
+  implements UserLearningObjectDatastore {
+  stubs = new Stubs();
+  searchReleasedUserObjects(
+    query: LearningObjectQuery,
+    username: string,
+  ): Promise<LearningObject[]> {
+    return Promise.resolve([this.stubs.learningObject]);
+  }
+  searchAllUserObjects(
+    query: UserLearningObjectSearchQuery,
+    username: string,
+    collectionRestrictions?: CollectionAccessMap,
+  ): Promise<LearningObject[]> {
+    return Promise.resolve([this.stubs.learningObject]);
+  }
 }
