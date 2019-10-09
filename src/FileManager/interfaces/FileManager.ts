4583c51f796730c3283db5baea0e0d390e768a43
pzalep1 <43140629+pzalep1@users.noreply.github.com>
2019-10-09 13:25:11 -0400

    Update src/FileManager/interfaces/FileManager.ts
    Co-Authored-By: Nick Visalli <32280549+nvisal1@users.noreply.github.com>

--- src/FileManager/interfaces/FileManager.ts
+++ src/FileManager/interfaces/FileManager.ts
@@ -26,7 +26,7 @@ export abstract class FileManager {
    * @abstract
    * @param {string} authorUsername [The Learning Object's author's username]
    * @param {string} learningObjectId [The id of the Learning Object to upload file to]
-   * @param {number} version [The version id of the Learning Object]
+   * @param {number} version [The version of the Learning Object]
    * @param {string} path [The path of the file to delete]
    * @returns {Promise<void>}
    * @memberof FileManager
