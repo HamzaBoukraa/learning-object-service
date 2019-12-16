7ee32de95d259ec528ac99d3b818f54e9fc4f45b
Nick Visalli <nvisal1@students.towson.edu>
2019-12-16 13:33:59 -0500

    Add Learning Object to search index during creation of editorial revision

--- src/Revisions/RevisionsDataStore.ts
+++ src/Revisions/RevisionsDataStore.ts
@@ -1,5 +1,4 @@
 import { LearningObject } from '../shared/entity';
-import { LearningObjectSummary } from '../shared/types';
 
 export interface RevisionsDataStore {
   createRevision(cuid: string, newRevisionId: number, revisionStatus?: LearningObject.Status.UNRELEASED | LearningObject.Status.PROOFING): Promise<void>;
