c3ad810da21d1eb0909d1d0bd6c5fdbc379fac89
Nick Visalli <32280549+nvisal1@users.noreply.github.com>
2019-12-20 09:31:49 -0500

    Fix bug where Learning Object was not inserted into search index on creation of editorial revision (#498)
    * Create bundle with CUID, set filename to lo name on download
    
    * Add Learning Object to search index during creation of editorial revision
    
    * 3.23.2
    
    * 3.23.3

--- src/Revisions/RevisionRouteHandler.ts
+++ src/Revisions/RevisionRouteHandler.ts
@@ -1,13 +1,11 @@
 import { Request, Response, Router } from 'express';
 import { mapErrorToResponseData } from '../shared/errors';
-import { DataStore } from '../shared/interfaces/DataStore';
-import { LibraryCommunicator } from '../shared/interfaces/interfaces';
-import { LearningObjectSummary, UserToken } from '../shared/types';
-import { LearningObject } from '../shared/entity';
-import { toBoolean } from '../shared/functions';
+import { UserToken } from '../shared/types';
 import * as RevisionInteractor from './interactors';
 import { RevisionsMongoDriver } from './RevisionsMongoDriver';
 import { RevisionsDataStore } from './RevisionsDataStore';
+import { ElasticsearchDriver } from './ElasticsearchDriver';
+import { RevisionsSearchIndex } from './RevisionsSearchIndex';
 
 /**
  * Initializes an Express router for the Revision resource.
@@ -18,10 +16,18 @@ import { RevisionsDataStore } from './RevisionsDataStore';
  */
 export function initializePrivate({ router }: { router: Router }) {
   const revisionsDataStore = new RevisionsMongoDriver();
+  const revisionsSearchIndex = new ElasticsearchDriver();
 
   const createRevision = async (req: Request, res: Response) => {
     try {
-      const params: { username: string, cuid: string, dataStore: RevisionsDataStore, requester: UserToken } = { ...req.params, dataStore: revisionsDataStore, requester: req.user };
+      const params: {
+        username: string,
+        cuid: string, dataStore: RevisionsDataStore, searchIndex: RevisionsSearchIndex, requester: UserToken } = {
+          ...req.params,
+          dataStore: revisionsDataStore,
+          searchIndex: revisionsSearchIndex,
+          requester: req.user,
+        };
 
       const revisionUri = await RevisionInteractor.createLearningObjectRevision(params);
       res.status(200).json({ revisionUri });
