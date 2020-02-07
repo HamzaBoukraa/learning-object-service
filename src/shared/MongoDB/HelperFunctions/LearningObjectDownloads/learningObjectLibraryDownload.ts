946e8c89be42dc86eb779eeeb90acf28fd8fec8b
pzalep1 <pz0342@gmail.com>
2020-02-07 13:22:55 -0500

    Send downloads to it's own collection

--- src/shared/MongoDB/HelperFunctions/LearningObjectDownloads/learningObjectLibraryDownload.ts
+++ src/shared/MongoDB/HelperFunctions/LearningObjectDownloads/learningObjectLibraryDownload.ts
@@ -1,6 +1,7 @@
 import { COLLECTIONS } from '../../../../drivers/MongoDriver';
 import { MongoConnector } from '../../../MongoDB/MongoConnector';
-import { LearningObject, LibraryItem } from '../../../entity';
+import { LearningObject } from '../../../entity';
+import { UserToken } from '../../../types';
 
 
 /**
@@ -8,10 +9,27 @@ import { LearningObject, LibraryItem } from '../../../entity';
  * @param username The username of the user downloading the object
  * @param id The learningObject ID of the learningObject being downloaded
  */
-export async function updateObjectInLibraryForDownload(
-    username: string,
+export async function updateDownloads(
+    user: UserToken,
     downloadedLearningObject: LearningObject,
 ): Promise<void> {
+    const downloadObject = cleanObject(user, downloadedLearningObject);
     const db = MongoConnector.client().db('cart-service');
-    await db.collection(COLLECTIONS.LIBRARY).updateOne({ savedBy: username, cuid: downloadedLearningObject.cuid }, { $set: {downloaded: true }});
+    await db.collection(COLLECTIONS.DOWNLOADS).insert(downloadObject);
+}
+
+
+function cleanObject(user: UserToken, object: LearningObject) {
+    const downloadObject = {
+        learningObjectCuid: object.cuid,
+        learningObjectVersion: object.version,
+        timestamp: Date.now(),
+        downloadedBy : {
+            username: user.username,
+            name: user.name,
+            email: user.email,
+            organization: user.organization,
+        },
+    };
+    return downloadObject;
 }
--- src/shared/MongoDB/HelperFunctions/index.ts
+++ src/shared/MongoDB/HelperFunctions/index.ts
@@ -9,7 +9,7 @@ import { generateLearningObjectSummary } from './generateLearningObjectSummary/g
 import { loadChildObjects } from './loadChildObjects/loadChildObjects';
 import { loadReleasedChildObjects } from './loadReleasedChildObjects/loadReleasedChildObjects';
 import { learningObjectHasRevision } from './learningObjectHasRevision/learningObjectHasRevision';
-import { updateObjectInLibraryForDownload } from './LearningObjectDownloads/learningObjectLibraryDownload';
+import { updateDownloads } from './LearningObjectDownloads/learningObjectLibraryDownload';
 
 export {
   bulkGenerateLearningObjects,
@@ -23,5 +23,5 @@ export {
   loadChildObjects,
   loadReleasedChildObjects,
   validatePageNumber,
-  updateObjectInLibraryForDownload,
+  updateDownloads,
 };
