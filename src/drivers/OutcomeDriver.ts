57ddc9df77db5578bc6dd369eec90e4ac45f2961
pzalep1 <pz0342@gmail.com>
2020-07-13 11:36:24 -0400

    Change request to use superagent

--- src/drivers/OutcomeDriver.ts
+++ src/drivers/OutcomeDriver.ts
@@ -1,5 +1,5 @@
 import { OUTCOME_SERVICE_ROUTES } from '../shared/routes';
-import * as request from 'request-promise';
+import * as request from 'superagent';
 import { generateServiceToken } from './TokenManager';
 import { ResourceError, ResourceErrorReason} from '../shared/errors';
 
@@ -16,9 +16,13 @@ export class OutcomeDriver {
 
     public async deleteAllOutcomes(id: string, username: string): Promise<void> {
         try {
-            const options = { ...this.options };
-            options.uri = OUTCOME_SERVICE_ROUTES.DELETE_ALL(id, username);
-            return request(options);
+            const authorization = `Bearer ${generateServiceToken()}`;
+            const response = await request
+                .delete(OUTCOME_SERVICE_ROUTES.DELETE_ALL(id, username))
+                .set('Accept', 'application/json')
+                .set('Authorization', authorization);
+
+            return response.body;
         } catch (e) {
             throw new ResourceError(`${e}`, ResourceErrorReason.BAD_REQUEST);
         }
